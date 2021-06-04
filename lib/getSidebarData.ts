84e17584b19f57cfb5419d54182e0d0552beff28
Deepankar Bhade <deep14bhade@gmail.com>
2021-06-04 11:08:07 +0530

    feat: script to generate files order

--- lib/getSidebarData.ts
+++ lib/getSidebarData.ts
@@ -1,11 +1,9 @@
+/* eslint-disable no-console */
 import fs from 'fs';
 import path from 'path';
 import  folderList  from "@/data/folder"
-import prettify from './prettify';
-
 
 const root = process.cwd();
-const folder = `docs`
 
 export interface SidebarDataType {
   topic: string;
@@ -19,14 +17,15 @@ const getSidebarData  = async ():  Promise<SidebarDataType[]> => {
   let finalData : SidebarDataType[]  = []
   folderList.forEach(el => {
     if(el!==null){
-      const files = fs.readdirSync(path.join(root, folder, el));
-      finalData = [...finalData , {
-        topic: el.replace('-',' ').toUpperCase(),
-        fileSlugs: files.map(e => ({
-            text: prettify(e),
-            slug: `/${el}/${e.replace('.mdx','')}`
-          }) )
-      }]
+      try {
+        const data = fs.readFileSync(path.join(root, 'data/files', `${el}.json`), 'utf8');
+        finalData = [...finalData , {
+          topic: el.replace('-',' ').toUpperCase(),
+          fileSlugs: JSON.parse(data)
+        }]
+      } catch(e) {
+          console.log('Error:', e.stack);
+      }
     }
   })
   return finalData;
