c9008b419bbf3de4329b0ab23fd3a1b3ca4c1839
Deepankar Bhade <deep14bhade@gmail.com>
2021-06-11 18:54:19 +0530

    chore: move v2 docs in /v2

--- data/folder.js
+++ data/folder.js
@@ -1,2 +1,2 @@
-const folderList = ['v2', 'server-side', 'client-sdks', 'web-frameworks', 'release-notes'];
+const folderList = ['100ms-v2', 'server-side', 'client-sdks', 'web-frameworks', 'release-notes'];
 module.exports = folderList;
