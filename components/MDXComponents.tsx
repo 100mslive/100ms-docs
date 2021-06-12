7339eb39b24663a96512970221923e59757527a4
Deepankar Bhade <deep14bhade@gmail.com>
2021-06-12 12:42:57 +0530

    feat: make note comp as blockquote

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -10,9 +10,12 @@ import Note from './Note';
 
 const CodeCustom = (props: any) => <Code id={nanoid(10)}>{props.children}</Code>;
 
+const NoteCustom = (props: any) => <Note type="success">{props.children}</Note>;
+
 const MDXComponents = {
     Note,
     Image,
+    blockquote: NoteCustom,
     code: CodeCustom,
     Code,
     DocLayout,
