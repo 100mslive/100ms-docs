1de3fc3e030454370ea26c517569f342a6d18864
Deepankar Bhade <deep14bhade@gmail.com>
2021-08-03 14:43:03 +0530

    feat: adding new sections

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,11 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import React from 'react';
+import ReactIcon from '@/assets/icons/ReactIcon';
+import JavascriptIcon from '@/assets/icons/JavascriptIcon';
+import AndroidIcon from '@/assets/icons/AndroidIcon';
+import IosIcon from '@/assets/icons/IosIcon';
+import ServerIcon from '@/assets/icons/ServerIcon';
 
 type NavRoute = {
     url: string;
@@ -19,14 +24,33 @@ const Sidebar: React.FC<Props> = ({ nav, menu }) => {
         <div className="ctx">
             {/* Sidebar Version Section */}
             <section className="menu-container">
-                <div className="menu-title">VERSIONS</div>
+                <div className="menu-title">TECHNOLOGY</div>
 
-                <a href="/v2/100ms-v2/Basics">
-                    <div className="menu-item">v2.0.0</div>
-                </a>
-                <a href="/v1/100ms-v1/basics">
-                    <div className="menu-item">v1.0.0</div>
-                </a>
+                <Link href="/react">
+                    <div className="menu-item">
+                        <ReactIcon /> REACT
+                    </div>
+                </Link>
+                <Link href="/javascript">
+                    <div className="menu-item">
+                        <JavascriptIcon /> JAVASCRIPT
+                    </div>
+                </Link>
+                <Link href="/android">
+                    <div className="menu-item">
+                        <AndroidIcon /> ANDROID
+                    </div>
+                </Link>
+                <Link href="/ios">
+                    <div className="menu-item">
+                        <IosIcon /> iOS
+                    </div>
+                </Link>
+                <Link href="/server-side">
+                    <div className="menu-item">
+                        <ServerIcon /> SERVER-SIDE
+                    </div>
+                </Link>
             </section>
 
             {/* Sidebar Menu Section */}
@@ -80,6 +104,8 @@ const Sidebar: React.FC<Props> = ({ nav, menu }) => {
                     color: var(--accents8);
                     font-weight: 600;
                     font-size: 14px;
+                    display: flex;
+                    align-items: center;
                 }
 
                 a {
