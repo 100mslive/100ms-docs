fb8a3875b45df4a5ffc282ec43c17b33168fe89e
Deepankar Bhade <deep14bhade@gmail.com>
2021-08-12 09:29:53 +0530

    add links to v1/v2 404 page

--- pages/v2/[...slug].tsx
+++ pages/v2/[...slug].tsx
@@ -5,16 +5,32 @@ const Post = () => {
     const router = useRouter();
 
     return (
-        <div>
+        <div className="page">
             <p>{router.asPath} does not exist</p>
             <h1>Our Docs has been moved:</h1>
             <p>Refer these Links:</p>
             <ul>
-                <li>Javascript</li>
-                <li>Android</li>
-                <li>iOS</li>
-                <li>Server-Side</li>
+                <li>
+                    <a href="/javascript">Javascript</a>
+                </li>
+                <li>
+                    <a href="/android">Android</a>
+                </li>
+                <li>
+                    <a href="/ios">iOS</a>
+                </li>
+                <li>
+                    {' '}
+                    <a href="/server-side">Server-Side</a>
+                </li>
             </ul>
+            <style jsx>{`
+                .page {
+                    padding-top: 100px;
+                    max-width: 500px;
+                    margin: 0 auto;
+                }
+            `}</style>
         </div>
     );
 };
