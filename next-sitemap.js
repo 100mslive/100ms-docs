5693c3ac8b9b4739ed203a834c11c80f8d46c625
Deepankar <statisticsaffinity@gmail.com>
2021-09-03 07:43:07 +0530

    Revert "feat: cmd palette used via keyboard"

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,3 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://docs.100ms.live/',
-    exclude: ['/api-reference/*']
+    siteUrl: process.env.SITE_URL || 'https://docs.100ms.live/'
 };
