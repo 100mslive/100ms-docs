1678f625dda27fe602118231d8e99687e2391e8a
Deepankar Bhade <deep14bhade@gmail.com>
2021-10-03 20:03:21 +0530

    chore: prettier and small fixes

--- components/Code.tsx
+++ components/Code.tsx
@@ -15,20 +15,20 @@ export const CopyIcon = () => (
         <path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z" />
     </svg>
 );
-export const Check = () => (
-    <svg 
-        width="24" 
-        height="24" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
+export const CheckIcon = () => (
+    <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="feather feather-check">
         <polyline points="20 6 9 17 4 12" />
     </svg>
-)
+);
 const Code: React.FC = ({ children }) => {
     const textRef = React.useRef(null);
     const copyFunction = () => {
@@ -50,17 +50,24 @@ const Code: React.FC = ({ children }) => {
     };
     return (
         <div className="code-block" onMouseEnter={onEnter} onMouseLeave={onExit}>
-            {hovered ? (
-                <button aria-label="Copy to Clipboard" onClick={() => copyFunction()} type="button">
+            {hovered && !copy ? (
+                <button
+                    aria-label="Copy to Clipboard"
+                    onClick={() => copyFunction()}
+                    type="button"
+                    className="copied">
                     <CopyIcon />
                 </button>
             ) : null}
-            {copy ? 
-                <div className="copied">
-                    <Check /> 
-                </div>
-            : null
-            }{' '}
+            {copy ? (
+                <button
+                    aria-label="Copy to Clipboard"
+                    onClick={() => copyFunction()}
+                    type="button"
+                    className="copied">
+                    <CheckIcon />
+                </button>
+            ) : null}{' '}
             <div ref={textRef}>{children}</div>
             <style jsx>{`
                 .code-block {
@@ -84,7 +91,7 @@ const Code: React.FC = ({ children }) => {
                     width: 25px;
                     height: 25px;
                     padding: 5px;
-                    border: 1px solid var(--gray6);
+                    border: 1px solid var(--gray3);
                     background: var(--gray1);
                     border-radius: 5px;
                     top: 10px;
