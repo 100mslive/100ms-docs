3e57ed20db9b45324d5f5081f998c4bc8e38b00a
somesh202 <77766761+somesh202@users.noreply.github.com>
2021-10-18 15:40:38 +0530

    improved note component

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -16,8 +16,66 @@ import DownloadCollection from './DownloadCollection';
 
 const CodeCustom = (props: any) => <Code>{props.children}</Code>;
 
-const NoteCustom = (props: any) => <Note type="success">{props.children}</Note>;
-
+const NoteCustom = (props: any) => (
+    <Note type="success" variant="blue">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            className="iconify iconify--ic"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24">
+            <path
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
+                fill="currentColor"></path>
+        </svg>
+        &nbsp;
+        {props.children}
+    </Note>
+);
+const NoteWarning = (props: any) => (
+    <Note type="warning" variant="yellow">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            className="iconify iconify--ic"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24">
+            <path
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
+                fill="currentColor"></path>
+        </svg>
+        &nbsp;
+        {props.children}
+    </Note>
+);
+const NoteError = (props: any) => (
+    <Note type="error" variant="red">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            className="iconify iconify--ic"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24">
+            <path
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 11c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"
+                fill="currentColor"></path>
+        </svg>
+        &nbsp;
+        {props.children}
+    </Note>
+);
 const TableCustom = (props: any) => (
     <div className="table-wrapper">
         <table>{props.children}</table>
@@ -56,6 +114,8 @@ const MDXComponents = {
     Note,
     Image,
     blockquote: NoteCustom,
+    NoteWarning,
+    NoteError,
     code: CodeCustom,
     table: TableCustom,
     Code,
--- components/Note.tsx
+++ components/Note.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 interface Props {
     type: 'warning' | 'success' | 'error';
+    variant: 'yellow' | 'blue' | 'red';
 }
 
-const Note: React.FC<Props> = ({ type = 'success', children }) => {
+const Note: React.FC<Props> = ({ type = 'success', variant='blue', children }) => {
     const resolveColor = () => `var(--${type})`;
+    const backgroundColor = () => `var(--${variant})`;
     return (
         <div>
             {children}
@@ -15,8 +17,12 @@ const Note: React.FC<Props> = ({ type = 'success', children }) => {
                     padding-left: 20px;
                     margin: 20px 0;
                     border-radius: 5px;
-                    border 1px solid var(--gray5);
+                    border 1px solid ${resolveColor()};
                     border-left: 5px solid ${resolveColor()};
+                    color: ${resolveColor()};
+                    display: flex;
+                    align-items: center;
+                    background: ${backgroundColor()};
                 }
             `}</style>
         </div>
