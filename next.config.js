d3092f59b0010a34ff6dcabb892fd4315c2e58a2
ritik <ritikagrawal0@gmail.com>
2022-09-05 18:23:05 +0530

    webpack config updated - no preact

--- next.config.js
+++ next.config.js
@@ -2,41 +2,39 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  basePath: "/docs",
-  webpack: (config, { isServer, dev }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+    basePath: '/docs',
+    webpack: (config, { isServer, dev }) => {
+        // Fixes npm packages that depend on `fs` module
+        // if (!isServer) {
+        //   config.node = {
+        //     fs: "empty",
+        //   };
+        // }
+        // if (!dev && !isServer) {
+        //   Object.assign(config.resolve.alias, {
+        //     react: "preact/compat",
+        //     "react-dom/test-utils": "preact/test-utils",
+        //     "react-dom": "preact/compat",
+        //   });
+        // }
 
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader",
-    });
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'raw-loader'
+        });
 
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/:path((?!docs).*)",
-        destination: `${
-          process.env.NEXT_PUBLIC_HMS_WEBSITE_DOMAIN || ""
-        }/docs/:path*`,
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
+        return config;
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path((?!docs).*)',
+                destination: `${process.env.NEXT_PUBLIC_HMS_WEBSITE_DOMAIN || ''}/docs/:path*`,
+                permanent: true,
+                basePath: false
+            }
+        ];
+    }
 };
 
 module.exports = nextConfig;
