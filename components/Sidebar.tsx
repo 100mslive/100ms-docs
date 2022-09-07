d587325f3eeb2483a8b756b4527a266a342945fc
Nixon Samuel <nixonsamuel@Nixons-MacBook-Pro.local>
2022-09-07 18:09:50 +0530

    added content for all sections

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -54,10 +54,10 @@ const Sidebar: React.FC<Props> = ({ nav, menu }) => {
             apiRef: 'https://pub.dev/documentation/hmssdk_flutter/latest/hmssdk_flutter/hmssdk_flutter-library.html'
         },
         {
-            link: '/server-side/v2/foundation/basics',
+            link: '/server-side/v2/introduction/overview',
             name: 'Server-Side',
             icon: <ServerIcon />,
-            apiRef: '/server-side/v2/foundation/basics'
+            apiRef: '/server-side/v2/introduction/overview'
         }
     ];
     // @ts-ignore
