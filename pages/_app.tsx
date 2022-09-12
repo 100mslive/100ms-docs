aeaf97c09bf3659dec9f88b9f8f97f67d886f6ee
hardik1003 <hardik@100ms.live>
2022-09-12 12:25:36 +0530

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import SEO from '../next-seo.config';
+import { currentUser } from '../lib/currentUser';
 import 'inter-ui/inter.css';
 import '@/styles/theme.css';
 import '@/styles/nprogress.css';
