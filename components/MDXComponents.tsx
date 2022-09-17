8d687e977bc8f7baef74f180236732733a77e4ff
ritik <ritikagrawal0@gmail.com>
2022-09-17 17:55:16 +0530

    fix: added analytics to server-side docs

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,26 +1,24 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
-import { Tabs, Tab } from './Tabs';
-import Code from './Code';
-import Note from './Note';
 import BaseRequest from './BaseRequest';
+import Code from './Code';
 import EndpointRequest from './EndpointRequest';
-import PostRequest from './PostRequest';
 import GetRequest from './GetRequest';
-
+import Note from './Note';
+import PostRequest from './PostRequest';
+import { Tab, Tabs } from './Tabs';
 import DeleteRequest from './DeleteRequest';
-
-import Response from './Response';
-import Request from './Request';
-import ResponseBox from './ResponseBox';
 import Codesandbox from './Codesandbox';
-import Text from './Text';
-import View from './View';
 import Content from './Content';
 import DownloadCollection from './DownloadCollection';
+import Request from './Request';
+import Response from './Response';
+import ResponseBox from './ResponseBox';
+import Text from './Text';
+import View from './View';
 
 const CodeCustom = (props: any) => <Code {...props}>{props.children}</Code>;
 
@@ -52,7 +50,10 @@ const LinkCustom = (props) => {
     }
 
     return (
-        <a target="_blank" rel="noopener noreferrer" href={href}>
+        <a target="_blank" rel="noopener noreferrer" href={href} onClick={() => window.analytics.track('link.clicked', {
+            btnId: typeof props?.children === typeof '' ? props?.children : props?.children?.props?.alt,
+            page: window?.location?.pathname
+        })}>
             {props.children}
         </a>
     );
