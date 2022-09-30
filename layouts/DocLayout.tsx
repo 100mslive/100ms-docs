63973cd4c9a3aecd6368a22a88680341f19d1c47
ritik <ritikagrawal0@gmail.com>
2022-09-30 17:13:35 +0530

    fix: sidebar scroll fixed

--- layouts/DocLayout.tsx
+++ layouts/DocLayout.tsx
@@ -1,84 +1,43 @@
-import EditFile from '@/components/EditFile';
+import React from 'react';
 import Header from '@/components/Header';
-import Pagination from '@/components/Pagination';
 import SegmentAnalytics from '@/components/SegmentAnalytics';
 import Sidebar from '@/components/Sidebar';
-import Toc from '@/components/Toc';
-import { PaginationType } from '@/lib/getPagination';
-import { scrollToUrlHash } from '@/lib/scrollToUrlHash';
 import useLockBodyScroll from '@/lib/useLockBodyScroll';
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
-import React from 'react';
 
-type NavRoute = {
-    url: string;
-    title: string;
-};
-
-export type AllDocsType = {
-    url: string;
-    title: string;
-    description: string;
-    nav: number;
-    content: string;
-};
-
-interface Props {
-    frontMatter: {
-        title: string;
-        nav: number;
-    };
-    nav: Record<string, Record<string, NavRoute>>;
-    pagination: {
-        previousPost: PaginationType;
-        nextPost: PaginationType;
-    };
-    allDocs: AllDocsType[];
-}
-
-const DocLayout: React.FC<Props> = ({ frontMatter, nav, children, pagination, allDocs }) => {
-    const router = useRouter();
+export default function Layout({ children }) {
+    const { frontMatter, allDocs } = children.props
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const router = useRouter() as any;
+    const {
+        query: { slug }
+    } = useRouter();
+    const [currentDocSlug] = slug as string[];
     const SEO = {
-        title: `${
-            frontMatter.title || '100ms Docs'
-        } | 100ms - Video conferencing infrastructure for a video-first world`,
+        title: `${frontMatter.title || '100ms Docs'
+            } | 100ms - Video conferencing infrastructure for a video-first world`,
         openGraph: {
-            title: `${
-                frontMatter.title || '100ms Docs'
-            } | 100ms - Video conferencing infrastructure for a video-first world`
+            title: `${frontMatter.title || '100ms Docs'
+                } | 100ms - Video conferencing infrastructure for a video-first world`
         },
-        canonical: `${process.env.NEXT_PUBLIC_CANONICAL_BASE_URL}${
-            router.asPath === '/' ? '' : router.asPath.split('?')[0]
-        }`
+        canonical: `${process.env.NEXT_PUBLIC_CANONICAL_BASE_URL}${router.asPath === '/' ? '' : router.asPath.split('?')[0]
+            }`
     };
     const [menu, setMenu] = React.useState(false);
     const [modal, setModal] = React.useState(false);
     const menuState = { menu, setMenu };
-    const [activeHeading, setActiveHeading] = React.useState('');
-    const [activeSubHeading, setActiveSubHeading] = React.useState('');
 
-    useLockBodyScroll(modal);
     let newNav;
-    // if 3 levels of directory
-    let showPagination = true;
-    // @ts-ignore
+    let { nav } = children.props
+    nav = nav[currentDocSlug]
     if (router.query.slug[0] !== 'v1' && router.query.slug[0] !== 'v2') {
-        // TODO: remove this ^ not needed if `v1` & `v2` docs are replaced
-        // @ts-ignore
         if (router.query.slug?.length > 3) {
-            // @ts-ignore
             newNav = nav[router.query.slug[1]];
-            // ? Case for `api-reference`
-            // @ts-ignore
             if (router.query.slug[0] === 'api-reference') {
-                // Don't show Pagination for Android
-                // @ts-ignore
-                if (router.query.slug[1] === 'android') {
-                    showPagination = false;
-                }
-                // object -> folder-> content
-                // @ts-ignore
+                // if (router.query.slug[1] === 'android') {
+                //     showPagination = false;
+                // }
                 newNav = nav[router.query.slug[1]][router.query.slug[2]];
             }
         }
@@ -86,50 +45,14 @@ const DocLayout: React.FC<Props> = ({ frontMatter, nav, children, pagination, al
         newNav = nav;
     }
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            scrollToUrlHash(router.asPath);
-        }, 500);
-    }, [router.asPath]);
-
-    React.useEffect(() => {
-        const getTopIndex = (arr) => {
-            for (let i = arr.length - 1; i >= 0; i--)
-                if (Math.floor(arr[i].getBoundingClientRect().top) < 200) return i;
-            return -1;
-        };
-
-        const getActiveLinks = () => {
-            const h2Array = document.getElementsByTagName('h2');
-            const h3Array = document.getElementsByTagName('h3');
-
-            const h2Index = getTopIndex(h2Array);
-            const h3Index = getTopIndex(h3Array);
-
-            if (h2Index >= 0) {
-                setActiveHeading(h2Array[h2Index].id);
-                if (
-                    h3Index >= 0 &&
-                    h3Array[h3Index].getBoundingClientRect().top >
-                        h2Array[h2Index].getBoundingClientRect().top
-                )
-                    setActiveSubHeading(h3Array[h3Index].id);
-                else setActiveSubHeading('');
-            }
-        };
-        getActiveLinks();
-        window.addEventListener('scroll', getActiveLinks);
-
-        return () => window.removeEventListener('scroll', getActiveLinks);
-    }, []);
-
+    useLockBodyScroll(modal);
     return (
         <>
-            <div className="page">
+            <div style={{ margin: "0" }}>
                 <NextSeo {...SEO} />
                 <SegmentAnalytics options={{}} title={frontMatter.title} />
                 <Header modal={modal} setModal={setModal} menuState={menuState} docs={allDocs} />
-                <div className="ctx">
+                <div className="ctx" >
                     <div className="content-wrapper">
                         <div
                             className="sidebar-container"
@@ -138,67 +61,27 @@ const DocLayout: React.FC<Props> = ({ frontMatter, nav, children, pagination, al
                             }}>
                             <Sidebar menu={menu} nav={newNav} />
                         </div>
-                        <article>
-                            <h1>{frontMatter.title}</h1>
-                            {children}
-                            <hr />
-                            {pagination.previousPost && showPagination && (
-                                <Pagination
-                                    next={pagination.nextPost}
-                                    prev={pagination.previousPost}
-                                />
-                            )}
-                            <EditFile slug={router.asPath} />
-                        </article>
-                        <Toc activeHeading={activeHeading} activeSubHeading={activeSubHeading} />
+                        {children}
                     </div>
                 </div>
-                <style jsx>{`
-                    html {
-                        height: 100%;
-                        scroll-behavior: smooth !important;
-                    }
-                    .page {
-                        margin: 0;
-                    }
-                    .ctx {
-                        display: flex;
-                        width: 100%;
-                        filter: blur(${modal ? '10px' : '0px'});
-                        background-color: var(--sidebar_bg) !important;
-                    }
-                    .sidebar-container {
-                        background-color: var(--sidebar_bg) !important;
-                    }
-                    .wrapper-ctx {
-                        display: flex;
-                    }
-                    article {
-                        max-width: 1200px;
-                        width: calc(100vw - 630px);
-                        flex-grow: 1;
-                        box-sizing: border-box;
-                        padding: 0 2rem;
-                        min-height: calc(100vh - 140px);
-                        padding-bottom: 80px;
-                        display: flex;
-                        flex-direction: column;
-                        align-items: stretch;
-                    }
-                    .content-wrapper {
-                        width: 100%;
-                        display: flex;
-                        justify-content: space-between;
-                        background-color: var(--article_bg);
-                    }
-                    .mobile-menu {
-                        display: none;
-                        position: absolute;
-                    }
-                `}</style>
             </div>
-        </>
-    );
-};
+            <style jsx>{`
+                 .ctx {
+                    display: flex;
+                    width: 100%;
+                    filter: blur(${modal ? '10px' : '0px'});
+                    background-color: var(--sidebar_bg) !important;
+                }
+                .content-wrapper {
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    background-color: var(--article_bg);
+                }
+                .sidebar-container {
+                    background-color: var(--sidebar_bg) !important;
+                }
+                `}</style>
+        </>)
+}
 
-export default DocLayout;
--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,40 +1,122 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import EditFile from '@/components/EditFile';
+import components from '@/components/MDXComponents';
+import Pagination from '@/components/Pagination';
+import Toc from '@/components/Toc';
+import DocLayout from '@/layouts/DocLayout';
+import getPagination from '@/lib/getPagination';
+import imagePlugin from '@/lib/image';
+import { DOCS_PATH, getAllDocs, getDocsPaths } from '@/lib/mdxUtils';
+import { scrollToUrlHash } from '@/lib/scrollToUrlHash';
+import withTableofContents from '@/lib/withTableofContents';
 import fs from 'fs';
 import matter from 'gray-matter';
 import mdxPrism from 'mdx-prism';
 import hydrate from 'next-mdx-remote/hydrate';
 import renderToString from 'next-mdx-remote/render-to-string';
+import { useRouter } from 'next/router';
 import path from 'path';
 import setValue from 'set-value';
-import components from '@/components/MDXComponents';
-import DocLayout from '@/layouts/DocLayout';
-import getPagination from '@/lib/getPagination';
-import imagePlugin from '@/lib/image';
-import { DOCS_PATH, getAllDocs, getDocsPaths } from '@/lib/mdxUtils';
-import withTableofContents from '@/lib/withTableofContents';
 
-const DocSlugs = ({ source, allDocs, nav, frontMatter }) => {
+
+
+const DocSlugs = ({ source, allDocs, frontMatter }) => {
     const {
         query: { slug }
     } = useRouter();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const router = useRouter() as any;
     const [currentDocSlug] = slug as string[];
+    const [activeHeading, setActiveHeading] = React.useState('');
+    const [activeSubHeading, setActiveSubHeading] = React.useState('');
     const currentDocs = allDocs.filter((doc) => doc.url.includes(`/${currentDocSlug}/`));
     const { previousPost, nextPost } = getPagination(currentDocs, slug as string[]);
     const pagination = { previousPost, nextPost };
     const content = hydrate(source, { components });
+
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            scrollToUrlHash(router.asPath);
+        }, 500);
+    }, [router.asPath]);
     React.useEffect(() => {
         if (!window.location.href.includes('#')) window.scrollTo(0, 0);
-    }, []);
+        const getTopIndex = (arr) => {
+            for (let i = arr.length - 1; i >= 0; i--)
+                if (Math.floor(arr[i].getBoundingClientRect().top) < 200) return i;
+            return -1;
+        };
+        const getActiveLinks = () => {
+            const h2Array = document.getElementsByTagName('h2');
+            const h3Array = document.getElementsByTagName('h3');
 
+            const h2Index = getTopIndex(h2Array);
+            const h3Index = getTopIndex(h3Array);
+
+            if (h2Index >= 0) {
+                setActiveHeading(h2Array[h2Index].id);
+                if (
+                    h3Index >= 0 &&
+                    h3Array[h3Index].getBoundingClientRect().top >
+                    h2Array[h2Index].getBoundingClientRect().top
+                )
+                    setActiveSubHeading(h3Array[h3Index].id);
+                else setActiveSubHeading('');
+            }
+        };
+        getActiveLinks();
+        window.addEventListener('scroll', getActiveLinks);
+
+        return () => window.removeEventListener('scroll', getActiveLinks);
+    }, []);
+    let showPagination = true;
+    // Don't show Pagination for Android
+    if (router.query.slug[1] === 'android') {
+        showPagination = false;
+    }
     return (
-        <DocLayout
-            frontMatter={frontMatter}
-            nav={nav[currentDocSlug]}
-            pagination={pagination}
-            allDocs={allDocs}>
-            {content}
-        </DocLayout>
+        <>
+            <article>
+                <h1>{frontMatter.title}</h1>
+                {content}
+                <hr />
+                {pagination.previousPost && showPagination && (
+                    <Pagination
+                        next={pagination.nextPost}
+                        prev={pagination.previousPost}
+                    />
+                )}
+                <EditFile slug={router.asPath} />
+            </article>
+            <Toc activeHeading={activeHeading} activeSubHeading={activeSubHeading} />
+            <style jsx>{`
+                 html {
+                     height: 100%;
+                     scroll-behavior: smooth !important;
+                 }
+                 .wrapper-ctx {
+                     display: flex;
+                 }
+                 article {
+                     max-width: 1200px;
+                     width: calc(100vw - 630px);
+                     flex-grow: 1;
+                     box-sizing: border-box;
+                     padding: 0 2rem;
+                     min-height: calc(100vh - 140px);
+                     padding-bottom: 80px;
+                     display: flex;
+                     flex-direction: column;
+                     align-items: stretch;
+                 }
+                 .mobile-menu {
+                     display: none;
+                     position: absolute;
+                 }
+             `}</style>
+
+        </>
     );
 };
 
@@ -106,3 +188,11 @@ export const getStaticPaths = async () => {
         fallback: false
     };
 };
+
+DocSlugs.getLayout = function getLayout(page) {
+    return (
+        <DocLayout>
+            {page}
+        </DocLayout>
+    )
+}
