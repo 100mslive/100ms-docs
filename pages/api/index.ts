1d70ccdab4caa0e7d9b0616f5ee3d5d5d9075024
ritik <ritikagrawal0@gmail.com>
2022-10-13 14:19:34 +0530

    fix: add api/ files to ts files

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,11 +1,12 @@
 import { readdirSync } from 'fs';
 import path from 'path';
+
 export default async function handler(req, res) {
-    //Find the absolute path of the json directory
+    // Find the absolute path of the json directory
     const jsonDirectory = path.join(process.cwd());
     const first = readdirSync(jsonDirectory, { withFileTypes: true });
-    const second = readdirSync(jsonDirectory + '/docs', { withFileTypes: true });
-    //Read the json data file data.jso
-    //Return the content of the data file in json format
+    const second = readdirSync(`${jsonDirectory}/docs`, { withFileTypes: true });
+    // Read the json data file data.jso
+    // Return the content of the data file in json format
     res.status(200).json({ first, second });
 }
--- pages/api/mainContent.ts
+++ pages/api/mainContent.ts
@@ -7,7 +7,7 @@ import { getAllDocs, getNavfromDocs } from '../../lib/mdxUtils';
 export default async function handler(req, res) {
     res.setHeader('Cache-Control', 's-maxage=669600'); // 31 days limit by vercel
     const { filter } = req.query;
-    let allDocs = getAllDocs();
+    let allDocs = getAllDocs() as unknown;
     let nav;
     if (!filter || filter === 'nav') nav = getNavfromDocs(allDocs);
     if (filter !== 'docs' && filter) allDocs = undefined;
