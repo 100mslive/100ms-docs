900226c9519078d96b04ed2949d14de45c6602bb
ritik <ritikagrawal0@gmail.com>
2022-10-13 14:43:49 +0530

    fix: add api/index.ts file

---
+++ pages/api/index.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({
+        ENV: process.env.VERCEL_ENV,
+        VERCEL_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF,
+        VERCEL_GIT_COMMIT_MESSAGE: process.env.VERCEL_GIT_COMMIT_MESSAGE,
+        VERCEL_GIT_REPO_SLUG: process.env.VERCEL_GIT_REPO_SLUG
+    });
+}
