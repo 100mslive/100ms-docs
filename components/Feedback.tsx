8953f1add093f1cac9783005a9a0f5919d13c217
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-12 18:14:12 +0530

    updates

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -13,35 +13,31 @@ const Feedback = () => {
     const feedBackRef = React.useRef<HTMLDivElement | null>();
     const inputRef = React.useRef<HTMLTextAreaElement | undefined>();
 
-    React.useEffect(() => {}, []);
-
     // @ts-ignore
     useClickOutside(feedBackRef, () => setShowTextBox(false));
 
+    React.useEffect(() => {
+        if (inputRef.current) inputRef.current.focus();
+    }, [clickedEmoji]);
+
     return (
         // @ts-ignore
-        <Box ref={feedBackRef} css={{ maxWidth: '200px', ml: '$8', mt: '$18' }}>
-            <Text css={{ fontWeight: '$medium', color: '$textHighEmp', fontSize: '13px' }}>
-                How helpful was this page?
+        <Box ref={feedBackRef} css={{ maxWidth: '200px' }}>
+            <hr />
+            <Text
+                variant="tiny"
+                css={{ fontWeight: 'bold', color: '$textHighEmp', textTransform: 'uppercase' }}>
+                Was this helpful?
             </Text>
             <Flex css={{ gap: '30px', p: '$9 0' }}>
                 {emojis.map((emoji) => (
                     <span
                         title="Share your feedback!"
-                        style={{ position: 'relative', width: '27px', height: '27px' }}
+                        style={{ position: 'relative', width: '24px', height: '24px' }}
                         key={emoji.score}
                         onClick={() => {
                             setShowTextBox(true);
                             setClickedEmoji(emoji.score);
-                            inputRef.current?.focus();
-                            window.analytics.track('docs.feedback.rating', {
-                                title: document.title,
-                                referrer: document.referrer,
-                                path: window.location.pathname,
-                                rating: clickedEmoji,
-                                timeStamp: new Date().toLocaleString(),
-                                ...currentUser()
-                            });
                         }}>
                         <img
                             className="emoji"
@@ -53,50 +49,66 @@ const Feedback = () => {
                     </span>
                 ))}
             </Flex>
-            {submitSuccessful ? (
-                <Text css={{ color: '$textAccentHigh', fontWeight: '$semiBold', fontSize: '13px' }}>
-                    Feedback successfully submitted. Thank you!
-                </Text>
-            ) : (
-                <div className="bottomContent">
-                    <textarea
-                        maxLength={140}
-                        placeholder="Please share your feedback"
-                        cols={20}
-                        rows={3}
-                        // @ts-ignore
-                        ref={inputRef}
-                        onChange={(e) => {
-                            setMessage(e.target.value);
-                        }}
-                        style={{
-                            background: 'none',
-                            fontSize: '13px',
-                            marginBottom: '18px',
-                            borderRadius: '4px'
-                        }}
-                    />
-                    <Button
-                        variant="primary"
-                        css={{ ml: 'auto', fontSize: '$sm', padding: '$3 $6' }}
-                        disabled={!message}
-                        onClick={() => {
-                            window.analytics.track('docs.feedback.message', {
-                                title: document.title,
-                                message,
-                                referrer: document.referrer,
-                                path: window.location.pathname,
-                                rating: clickedEmoji,
-                                timeStamp: new Date().toLocaleString(),
-                                ...currentUser()
-                            });
-                            setSubmitSuccessful(true);
+            <Box css={{ position: 'relative' }}>
+                {submitSuccessful ? (
+                    <Text
+                        variant="xs"
+                        css={{
+                            color: '$textAccentHigh',
+                            fontWeight: '$semiBold',
+                            textAlign: 'center'
                         }}>
-                        Submit
-                    </Button>
-                </div>
-            )}
-
+                        Feedback successfully submitted. Thank you!
+                    </Text>
+                ) : (
+                    <div
+                        className="bottomContent"
+                        style={{
+                            opacity: showTextBox ? '1' : '0',
+                            top: showTextBox ? '0' : '-16px'
+                        }}>
+                        <textarea
+                            maxLength={140}
+                            placeholder="Please share your feedback"
+                            cols={20}
+                            rows={3}
+                            // @ts-ignore
+                            ref={inputRef}
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                            style={{
+                                background: 'none',
+                                fontSize: '13px',
+                                marginBottom: '18px',
+                                borderRadius: '4px'
+                            }}
+                        />
+                        <Button
+                            variant="primary"
+                            css={{
+                                ml: 'auto',
+                                fontSize: '$sm',
+                                padding: '$3 $6',
+                                cursor: 'pointer'
+                            }}
+                            onClick={() => {
+                                window.analytics.track('docs.feedback.message', {
+                                    title: document.title,
+                                    message,
+                                    referrer: document.referrer,
+                                    path: window.location.pathname,
+                                    rating: clickedEmoji,
+                                    timeStamp: new Date().toLocaleString(),
+                                    ...currentUser()
+                                });
+                                setSubmitSuccessful(true);
+                            }}>
+                            Submit
+                        </Button>
+                    </div>
+                )}
+            </Box>
             <style jsx>{`
                 .emoji {
                     filter: grayscale(100%);
@@ -106,20 +118,20 @@ const Feedback = () => {
                 .emoji:hover {
                     filter: none;
                     cursor: pointer;
-                    transform: scale(1.1);
+                    transform: scale(1.05);
                 }
 
                 img.emoji[data-active='true'] {
                     filter: none;
-                    transform: scale(1.2);
+                    transform: scale(1.08);
                 }
 
                 .bottomContent {
                     display: flex;
+                    position: relative;
+                    opacity: 0;
+                    transition: opacity, top 0.3s;
                     flex-direction: column;
-                    transform: scaleY(${showTextBox ? '1' : '0'});
-                    visibility: ${showTextBox ? 'visible' : 'collapse'};
-                    transition: transform 200ms ease;
                     margin-bottom: 16px;
                 }
 
