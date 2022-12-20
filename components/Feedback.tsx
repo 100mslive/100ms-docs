caff65a2a3cbec6bc97840b92db3b2c748aaf7a0
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2022-12-20 11:26:48 +0530

    GG-49 Update feedback elements event (#992)
    * update behaviour

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -8,6 +8,7 @@ const emojis = [{ score: 1 }, { score: 2 }, { score: 3 }, { score: 4 }];
 const Feedback = () => {
     const [showTextBox, setShowTextBox] = React.useState(false);
     const [clickedEmoji, setClickedEmoji] = React.useState(0);
+    const [firstSelection, setFirstSelection] = React.useState(0);
     const [submitSuccessful, setSubmitSuccessful] = React.useState(false);
     const [message, setMessage] = React.useState('');
     const feedBackRef = React.createRef<HTMLDivElement>();
@@ -15,9 +16,13 @@ const Feedback = () => {
 
     const getPlaceholder = {
         1: 'What should we fix?',
+        'title-1': 'Needs work',
         2: 'How can we make it better?',
+        'title-2': `Decent`,
         3: 'How can we make it even better?',
-        4: 'Great! What did you like?'
+        'title-3': `Nice`,
+        4: 'Great! What did you like?',
+        'title-4': `Awesome!`
     };
 
     useClickOutside(feedBackRef, () => setShowTextBox(false));
@@ -31,19 +36,30 @@ const Feedback = () => {
             <Text
                 variant="body2"
                 css={{
-                    color: 'var(--text_docs_primary)',
+                    color: 'var(--text_docs_primary)'
                 }}>
                 Was this helpful?
             </Text>
             <Flex justify="between" css={{ p: '$9 0' }}>
-                {emojis.map((emoji) => (
+                {emojis.map((emoji, id) => (
                     <span
-                        title="Share your feedback!"
+                        title={getPlaceholder[`title-${id + 1}`]}
                         style={{ position: 'relative', width: '24px', height: '24px' }}
                         key={emoji.score}
                         onClick={() => {
-                            setShowTextBox(true);
+                            if (showTextBox === false) {
+                                window.analytics.track('docs.feedback.rating', {
+                                    title: document.title,
+                                    referrer: document.referrer,
+                                    path: window.location.pathname,
+                                    rating: emoji.score,
+                                    timeStamp: new Date().toLocaleString(),
+                                    ...currentUser()
+                                });
+                                setFirstSelection(emoji.score);
+                            }
                             setClickedEmoji(emoji.score);
+                            setShowTextBox(true);
                         }}>
                         <img
                             className="emoji"
@@ -100,10 +116,10 @@ const Feedback = () => {
                             onClick={() => {
                                 window.analytics.track('docs.feedback.message', {
                                     title: document.title,
-                                    message,
+                                    message: message || '',
+                                    rating: firstSelection,
                                     referrer: document.referrer,
                                     path: window.location.pathname,
-                                    rating: clickedEmoji,
                                     timeStamp: new Date().toLocaleString(),
                                     ...currentUser()
                                 });
