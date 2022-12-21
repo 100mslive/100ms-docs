de4da467f139a2c91412d7e14a642320325a9b43
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-21 09:34:28 +0530

    set up instantsearch

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { SearchIcon, ChevronRightIcon } from '@100mslive/react-icons';
-import { Flex, Box, Text } from '@100mslive/react-ui';
+import { ChevronRightIcon } from '@100mslive/react-icons';
+import { Box, Text } from '@100mslive/react-ui';
 import useClickOutside from '@/lib/useClickOutside';
-import useKeyPress from '@/lib/useKeyPress';
-import useSearch from '@/lib/useSearch';
 import Link from 'next/link';
 
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('5UAX3T19GE', '6b2fcf18157b00a2c7f33452512da0ba');
+
 interface SearchModalProps {
-    docs: { url: string; title: string; description: string; nav: number; content: string }[];
     setModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
@@ -77,69 +79,13 @@ const ResultBox: React.FC<ResultBoxProps> = ({ title, url }) => {
     );
 };
 
-const SearchModal: React.FC<SearchModalProps> = ({ docs, setModal }) => {
-    const paletteTrack = React.useRef(-1);
-    const [search, setSearch] = React.useState('');
+const SearchModal: React.FC<SearchModalProps> = ({ setModal }) => {
     const ref = React.createRef<HTMLDivElement>();
-    const inputRef = React.createRef<HTMLInputElement>();
-
-    const res = useSearch({
-        search,
-        docs
-    });
 
     useClickOutside(ref, () => {
-        if (inputRef.current)
-            window.analytics.track('docs.search.dismissed', {
-                textInSearch: inputRef.current?.value || '',
-                totalNumberOfResults: res.length,
-                referrer: document.referrer,
-                path: window.location.hostname,
-                pathname: window.location.pathname,
-                href: window.location.href
-            });
         setModal(false);
     });
 
-    // reset if result is 0
-    if (res.length === 0) {
-        paletteTrack.current = -1;
-    }
-
-    const downKeyPressed = useKeyPress('ArrowDown');
-    const upKeyPressed = useKeyPress('ArrowUp');
-    if (downKeyPressed) {
-        // reset to top
-        if (paletteTrack.current >= res.length - 1) {
-            paletteTrack.current = 0;
-            const last = document.getElementById(`res-box-${res.length - 1}`);
-            if (last) last.style.backgroundColor = 'var(--surface_default)';
-        } else paletteTrack.current += 1;
-
-        const ele = document.getElementById(`res-box-${paletteTrack.current}`);
-        if (ele) {
-            ele.style.backgroundColor = 'var(--surface_light)';
-            ele.focus();
-        }
-        const prev = document.getElementById(`res-box-${paletteTrack.current - 1}`);
-        if (prev) prev.style.backgroundColor = 'var(--surface_default)';
-    }
-    if (upKeyPressed) {
-        // on top
-        if (paletteTrack.current === 0) {
-            paletteTrack.current = res.length - 1;
-            const top = document.getElementById(`res-box-0`);
-            if (top) top.style.backgroundColor = 'var(--surface_default)';
-        } else paletteTrack.current -= 1;
-
-        const ele = document.getElementById(`res-box-${paletteTrack.current}`);
-        if (ele) {
-            ele.style.backgroundColor = 'var(--surface_light)';
-            ele.focus();
-        }
-        const prev = document.getElementById(`res-box-${paletteTrack.current + 1}`);
-        if (prev) prev.style.backgroundColor = 'var(--surface_default)';
-    }
     return (
         <Box
             css={{
@@ -151,109 +97,11 @@ const SearchModal: React.FC<SearchModalProps> = ({ docs, setModal }) => {
                 bg: 'rgba(0, 0, 0, 0.8)'
             }}>
             <div className="search-modal" ref={ref}>
-                <Flex
-                    align="center"
-                    css={{
-                        color: '$textHighEmp',
-                        bg: '$surfaceDefault',
-                        padding: '12px 16px',
-                        border: '2.5px solid $primaryDefault',
-                        borderRadius: '0.5rem',
-                        margin: '0 auto',
-                        height: '20px'
-                    }}
-                    onClick={(e) => e.stopPropagation()}>
-                    <SearchIcon style={{ color: 'inherit', height: '30px', width: '30px' }} />
-                    <input
-                        ref={inputRef}
-                        value={search}
-                        type="text"
-                        autoFocus
-                        onChange={(e) => setSearch(e.target.value)}
-                        style={{
-                            marginLeft: '13px',
-                            backgroundColor: 'inherit',
-                            outline: 'none',
-                            border: 'none',
-                            height: '24px',
-                            width: '100%',
-                            fontWeight: '500',
-                            fontSize: '15px'
-                        }}
-                    />
-                    <Flex align="center" gap="2">
-                        <Flex
-                            align="center"
-                            css={{
-                                fontWeight: '$semiBold',
-                                fontSize: '$sm',
-                                backgroundColor: '$surfaceLight',
-                                color: '$textMedEmp',
-                                borderRadius: '4px',
-                                padding: '0 4px'
-                            }}>
-                            esc
-                        </Flex>
-                        <Text variant="xs" css={{ whiteSpace: 'nowrap', color: '$textMedEmp' }}>
-                            to close
-                        </Text>
-                    </Flex>
-                </Flex>
-                {res.length > 0 ? (
-                    <Box
-                        css={{
-                            position: 'relative',
-                            top: '$8',
-                            backgroundColor: '$surfaceDefault',
-                            border: '1px solid',
-                            borderColor: '$borderDefault',
-                            borderRadius: '$1',
-                            px: '$2',
-                            py: '$3 '
-                        }}>
-                        <Box
-                            css={{
-                                maxHeight: '60vh',
-                                overflow: 'auto'
-                            }}>
-                            {res.map((searchResult, i) => (
-                                <Box
-                                    key={searchResult.url}
-                                    css={{
-                                        borderColor: '$borderDefault',
-                                        '&:hover': { backgroundColor: '$surfaceLight' }
-                                    }}>
-                                    <Link href={searchResult.url} passHref>
-                                        <a
-                                            id={`res-box-${i}`}
-                                            className="res-box"
-                                            onClick={() => {
-                                                window.analytics.track(
-                                                    'docs.search.result.clicked',
-                                                    {
-                                                        totalNumberOfResults: res.length,
-                                                        textInSearch:
-                                                            inputRef?.current?.value || '',
-                                                        rankOfSearchResult: i + 1,
-                                                        locationOfSearchResult: searchResult.url,
-                                                        referrer: document.referrer,
-                                                        path: window.location.hostname,
-                                                        pathname: window.location.pathname
-                                                    }
-                                                );
-                                                setModal(false);
-                                            }}>
-                                            <ResultBox
-                                                title={searchResult.title}
-                                                url={searchResult.url}
-                                            />
-                                        </a>
-                                    </Link>
-                                </Box>
-                            ))}
-                        </Box>
-                    </Box>
-                ) : null}
+                <InstantSearch searchClient={searchClient} indexName="test">
+                    <SearchBox translations={{ placeholder: 'Search through docs' }} showLoadingIndicator/>
+                    <Hits />
+                </InstantSearch>
+
                 <style jsx>{`
                     .search-modal {
                         max-width: 600px;
