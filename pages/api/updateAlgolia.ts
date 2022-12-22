fa8538115718c28729dc822b2292463e9ef01998
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-22 16:33:34 +0530

    fix

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -5,12 +5,8 @@ import url from 'url'
 import algoliasearch from 'algoliasearch/lite';
 import updateIndex from '../../algolia/getRecords'
 
-const client = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "", process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY|| "");
-
-const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX|| "");
-
 const cors = Cors({
-    methods: ['POST']
+    methods: ['GET', "HEAD"]
 });
 
 function runMiddleware(req, res, fn) {
@@ -31,6 +27,10 @@ export default async function handler(req, res) {
     const dummyVar = fs.readdirSync(`${jsonDirectory}/common`)
     // @ts-ignore
     const dummyLink = url.pathToFileURL(path.resolve(`${jsonDirectory}/common`))
+    const client = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "", process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY|| "");
+
+    const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX|| "");
+
     const records = await updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`);
     // @ts-ignore
     index.replaceAllObjects(records).then(() => res.status(200).json({ status: "completed" }))   
