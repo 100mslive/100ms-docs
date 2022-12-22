f3df4132075e1373387bc73299a27e90e19f7fe8
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-22 17:11:29 +0530

    fix

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import url from 'url'
 import algoliasearch from 'algoliasearch/lite';
-import updateIndex from '../../algolia/getRecords'
+import updateIndex from '@/lib/algolia/getRecords'
 
 const cors = Cors({
     methods: ['GET', "HEAD"]
@@ -31,11 +31,13 @@ export default async function handler(req, res) {
     const adminKey = process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY
     const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX
     if(appId && adminKey && algoliaIndex){
-    const client = algoliasearch(appId, adminKey);
-    const index = client.initIndex(algoliaIndex);
-    const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`);
-    // @ts-ignore
-    await index.replaceAllObjects(records);
+        const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`);
+        console.log(records[0]);
+        const client = algoliasearch(appId, adminKey);
+        const index = client.initIndex(algoliaIndex);
+        console.log("Before API call")
+        // @ts-ignore
+        await index.replaceAllObjects(records);
         res.status(200).json({ status: "completed" })
     }else {
         res.status(200).json({ status: "failed" })
