e9d2cfed9e38da56bde21a0ceeea28d0aa9850fc
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-22 18:36:18 +0530

    URL fix

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -4,9 +4,10 @@ import fs from 'fs';
 import url from 'url'
 import algoliasearch from 'algoliasearch/lite';
 import updateIndex from '@/lib/algolia/getRecords'
+import { DOCS_PATH } from '@/lib/mdxUtils';
 
 const cors = Cors({
-    methods: ['GET', "HEAD"]
+    methods: ['GET', 'HEAD']
 });
 
 function runMiddleware(req, res, fn) {
@@ -31,13 +32,12 @@ export default async function handler(req, res) {
     const adminKey = process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY
     const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX
     if(appId && adminKey && algoliaIndex){
-        const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`);
+        const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`, `${jsonDirectory}\\docs`);
         console.log(records[0]);
         const client = algoliasearch(appId, adminKey);
         const index = client.initIndex(algoliaIndex);
-        console.log("Before API call")
         // @ts-ignore
-        await index.replaceAllObjects(records);
+        // await index.replaceAllObjects(records);
         res.status(200).json({ status: "completed" })
     }else {
         res.status(200).json({ status: "failed" })
