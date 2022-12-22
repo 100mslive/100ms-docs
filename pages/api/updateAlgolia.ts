1589e9d10e25360e4e188957652703c7644d3c2c
ritik <ritikagrawal0@gmail.com>
2022-12-22 17:51:16 +0530

    fix: link

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -1,12 +1,12 @@
 import Cors from 'cors';
 import path from 'path';
 import fs from 'fs';
-import url from 'url'
+import url from 'url';
 import algoliasearch from 'algoliasearch/lite';
-import updateIndex from '@/lib/algolia/getRecords'
+import updateIndex from '@/lib/algolia/getRecords';
 
 const cors = Cors({
-    methods: ['GET', "HEAD"]
+    methods: ['GET', 'HEAD']
 });
 
 function runMiddleware(req, res, fn) {
@@ -24,22 +24,22 @@ export default async function handler(req, res) {
     await runMiddleware(req, res, cors);
     const jsonDirectory = path.join(process.cwd());
     // @ts-ignore
-    const dummyVar = fs.readdirSync(`${jsonDirectory}/common`)
+    const dummyVar = fs.readdirSync(`${jsonDirectory}/common`);
     // @ts-ignore
-    const dummyLink = url.pathToFileURL(path.resolve(`${jsonDirectory}/common`))
-    const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
-    const adminKey = process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY
-    const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX
-    if(appId && adminKey && algoliaIndex){
+    const dummyLink = url.pathToFileURL(path.resolve(`${jsonDirectory}/common`));
+    const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
+    const adminKey = process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY;
+    const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX;
+    if (appId && adminKey && algoliaIndex) {
         const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`);
         console.log(records[0]);
         const client = algoliasearch(appId, adminKey);
         const index = client.initIndex(algoliaIndex);
-        console.log("Before API call")
+        console.log('Before API call');
         // @ts-ignore
         await index.replaceAllObjects(records);
-        res.status(200).json({ status: "completed" })
-    }else {
-        res.status(200).json({ status: "failed" })
+        res.status(200).json({ status: 'completed' });
+    } else {
+        res.status(200).json({ status: 'failed' });
     }
 }
