6caf5ac7371c202b8e84005b6f6307a7be3e8748
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-23 11:07:47 +0530

    update build command

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -1,44 +1,14 @@
-import Cors from 'cors';
 import path from 'path';
 import fs from 'fs';
 import url from 'url';
-import algoliasearch from 'algoliasearch/lite';
 import updateIndex from '@/lib/algolia/getRecords';
 
-const cors = Cors({
-    methods: ['GET', 'HEAD']
-});
-
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
-            if (result instanceof Error) {
-                return reject(result);
-            }
-            return resolve(result);
-        });
-    });
-}
-
-export default async function handler(req, res) {
-    await runMiddleware(req, res, cors);
+export default async function handler(_req, res) {
     const jsonDirectory = path.join(process.cwd());
     // @ts-ignore
     const dummyVar = fs.readdirSync(`${jsonDirectory}/common`);
     // @ts-ignore
     const dummyLink = url.pathToFileURL(path.resolve(`${jsonDirectory}/common`));
-    const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
-    const adminKey = process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_API_KEY;
-    const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX;
-    if (appId && adminKey && algoliaIndex) {
-        const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`, `${url.pathToFileURL(jsonDirectory).toString()}/docs`);
-        // console.log(records[1]);
-        const client = algoliasearch(appId, adminKey);
-        const index = client.initIndex(algoliaIndex);
-        // @ts-ignore
-        await index.replaceAllObjects(records);
-        res.status(200).json({ status: 'completed' });
-    } else {
-        res.status(200).json({ status: 'failed' });
-    }
+    const completedUpdate = await updateIndex();
+    res.status(200).json({ status: completedUpdate });     
 }
