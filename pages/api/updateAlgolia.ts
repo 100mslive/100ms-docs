8607ef4b112bb1681a094042ac3950b88315b1f8
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2022-12-23 09:17:55 +0530

    fix contentAlias

--- pages/api/updateAlgolia.ts
+++ pages/api/updateAlgolia.ts
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX;
     if (appId && adminKey && algoliaIndex) {
         const records = updateIndex(`${jsonDirectory}/common`, `${jsonDirectory}/docs`, `${url.pathToFileURL(jsonDirectory).toString()}/docs`);
-        // console.log(records[0]);
+        // console.log(records[1]);
         const client = algoliasearch(appId, adminKey);
         const index = client.initIndex(algoliaIndex);
         // @ts-ignore
