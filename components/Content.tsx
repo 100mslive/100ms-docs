d06b594c0466d7bf3c5d16c25d92940ea31303d7
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-01-16 09:26:55 +0530

    fix network usage

--- components/Content.tsx
+++ components/Content.tsx
@@ -6,15 +6,12 @@ import React from 'react';
 // Please use the same name as the md file to ensure
 // it gets picked up by the script for updating algolia records
 import Basics from '../common/basics.md';
-import Network from '../common/network.md';
 import Token from '../common/token.md';
 import TemplatesAndRoles from '../common/templates-and-roles.md';
 import SecurityTokens1 from '../common/security-tokens-1.md';
 import SecurityTokens2 from '../common/security-tokens-2.md';
 import LiveStreaming from '../common/live-streaming.md';
 import Recordings from '../common/recordings.md';
-import AdaptiveBitrate1 from '../common/adaptive-bitrate-1.md';
-import AdaptiveBitrate2 from '../common/adaptive-bitrate-2.md';
 import AndroidSdkVersionShield from '../common/android-sdk-version-shield.md';
 import BulkRoleChangeErrors from '../common/bulk-role-change-errors.md';
 import RtmpExternalConfig from '../common/rtmp-external-config.md';
@@ -32,13 +29,7 @@ const data = {
     recordings: Recordings,
     rtmpExternalConfig: RtmpExternalConfig,
     androidSdkVersionShield: AndroidSdkVersionShield,
-    bulkRoleChangeErrors: BulkRoleChangeErrors,
-    /**
-     * Adaptive bitrate docs are split into 2 parts because 3-backtick
-     * code snippets in `md` files are not rendering well in mdx
-     */
-    adaptiveBitrate1: AdaptiveBitrate1,
-    adaptiveBitrate2: AdaptiveBitrate2
+    bulkRoleChangeErrors: BulkRoleChangeErrors
 };
 
 interface Props {
