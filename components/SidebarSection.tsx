317295d7a639603e28dd5bc772313af8680eaf6e
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-01-19 16:41:22 +0530

    accordion file logic

--- components/SidebarSection.tsx
+++ components/SidebarSection.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { ChevronRightIcon, MinusIcon } from '@100mslive/react-icons';
 import { Flex, Text } from '@100mslive/react-ui';
+import ConditionalLink from './ConditionalLink';
 
 interface Props {
     value: String;
@@ -66,67 +67,73 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children, nested =
         }, 0);
     }, [activeItem]);
 
+    const indexURL = children?.index?.url || '';
+    // if (indexURL !== '#') delete children.index;
+
     return renderComponents ? (
         <section
-            className="menu-container"
             style={{
                 margin: nested ? '0 0 0 0.95rem' : '2px 0.5rem 0.5rem 0.25rem',
                 borderLeft: nested ? '2px solid var(--docs_border_strong' : 'none'
             }}
             key={`${key}-${index}`}>
-            <Flex
-                align="center"
-                onClick={() => {
-                    setOpenSection((prev) => {
-                        const currentList = JSON.parse(
-                            sessionStorage.getItem('openedAccordions') || '[]'
-                        );
-                        const updatedList = [
-                            ...new Set(
-                                prev === false
-                                    ? [...currentList, key]
-                                    : currentList.filter((heading) => heading !== key)
-                            )
-                        ];
-                        sessionStorage.setItem('openedAccordions', JSON.stringify(updatedList));
-                        return !prev;
-                    });
-                }}
-                css={{
-                    padding: '0 0 0.25rem 1rem',
-                    paddingTop: nested ? '0.125rem' : '0.5rem',
-                    margin: '0',
-                    cursor: 'pointer',
-                    borderRadius: '$0',
-                    width: 'max-content',
-                    color: openSection
-                        ? '$textHighEmp'
-                        : nested
-                        ? 'var(--docs_text_secondary)'
-                        : '$textMedEmp',
-                    '&:hover': { color: 'var(--docs_text_primary)' }
-                }}>
-                <ChevronRightIcon
-                    style={{
-                        height: '16px',
-                        width: '14px',
-                        marginRight: '0.5rem',
-                        transition: 'all 0.2s ease',
-                        transform: openSection ? 'rotateZ(90deg)' : ''
+            <ConditionalLink link={indexURL}>
+                <Flex
+                    align="center"
+                    onClick={() => {
+                        setOpenSection((prev) => {
+                            const currentList = JSON.parse(
+                                sessionStorage.getItem('openedAccordions') || '[]'
+                            );
+                            const updatedList = [
+                                ...new Set(
+                                    prev === false
+                                        ? [...currentList, key]
+                                        : currentList.filter((heading) => heading !== key)
+                                )
+                            ];
+                            sessionStorage.setItem('openedAccordions', JSON.stringify(updatedList));
+                            return !prev;
+                        });
                     }}
-                />
-                <Text
                     css={{
-                        color: 'inherit',
-                        fontWeight: openSection ? '600' : '400',
-                        fontSize: nested ? '13px' : '15px'
+                        padding: '0 0 0.25rem 1rem',
+                        paddingTop: nested ? '0.125rem' : '0.5rem',
+                        margin: '0',
+                        cursor: 'pointer',
+                        borderRadius: '$0',
+                        width: 'max-content',
+                        color: openSection
+                            ? '$textHighEmp'
+                            : nested
+                            ? 'var(--docs_text_secondary)'
+                            : '$textMedEmp',
+                        '&:hover': { color: 'var(--docs_text_primary)' }
                     }}>
-                    {toPascalCase(key.replace(/-/g, ' '))}
-                </Text>
-            </Flex>
+                    <ChevronRightIcon
+                        style={{
+                            height: '16px',
+                            width: '14px',
+                            marginRight: '0.5rem',
+                            transition: 'all 0.2s ease',
+                            transform: openSection ? 'rotateZ(90deg)' : ''
+                        }}
+                    />
+                    <Text
+                        css={{
+                            color: 'inherit',
+                            fontWeight: openSection ? '600' : '400',
+                            fontSize: nested ? '13px' : '15px'
+                        }}>
+                        {toPascalCase(key.replace(/-/g, ' '))}
+                    </Text>
+                </Flex>
+            </ConditionalLink>
             <div className={`accordion-content ${openSection ? 'active' : ''}`}>
                 {Object.entries(children as {}).map(([_, route]: [string, any]) =>
-                    Object.prototype.hasOwnProperty.call(route, 'title') ? (
+                    // eslint-disable-next-line no-nested-ternary
+                    Object.prototype.hasOwnProperty.call(route, 'title') &&
+                    route.url !== indexURL ? (
                         <Link prefetch={false} href={route.url || ''} key={`${route.url}-${index}`}>
                             <a
                                 ref={route.url === asPath ? activeItem : null}
@@ -153,26 +160,27 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children, nested =
                                 {route.title}
                             </a>
                         </Link>
-                    ) : (
+                    ) : indexURL !== route.url ? (
                         <SidebarSection index={index} value={_} nested>
                             {route}
                         </SidebarSection>
-                    )
+                    ) : null
                 )}
                 {key === 'features' && slug[0] !== 'server-side' ? (
                     <>
                         {aliasMenu.map((a) => (
                             <Link scroll={false} prefetch={false} href={a.url} key={a.url}>
                                 <a
+                                    ref={a.url === asPath ? activeItem : null}
                                     style={{
                                         cursor: 'pointer',
-                                        paddingTop: '0.5rem',
+                                        padding: '0.25rem 0',
                                         color:
                                             a.url === asPath
                                                 ? 'var(--docs_text_primary)'
                                                 : 'var(--docs_text_secondary)',
                                         fontWeight: a.url === asPath ? '500' : '400',
-                                        fontSize: '14px',
+                                        fontSize: '13px',
                                         lineHeight: '24px',
                                         borderLeft:
                                             a.url === asPath
@@ -180,9 +188,10 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children, nested =
                                                 : '2px solid var(--docs_border_strong)',
                                         display: 'flex',
                                         alignItems: 'center',
-                                        paddingLeft: '0.75rem',
-                                        marginLeft: '1rem'
+                                        paddingLeft: '1rem',
+                                        marginLeft: '0.95rem'
                                     }}>
+                                    <MinusIcon style={{ width: '12px', marginRight: '0.625rem' }} />
                                     {a.title}
                                 </a>
                             </Link>
