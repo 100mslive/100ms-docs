1d06945fc5b27a30ddf10825c23d6225689139e1
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-01-19 21:07:24 +0530

    fix

--- components/SidebarSection.tsx
+++ components/SidebarSection.tsx
@@ -4,7 +4,6 @@ import { ChevronRightIcon } from '@100mslive/react-icons';
 import { Flex, Text } from '@100mslive/react-ui';
 import SidebarItem from './SidebarItem';
 import ConditionalLink from './ConditionalLink';
-// import { toPascalCase } from '../lib/mdxUtils';
 
 interface Props {
     value: String;
@@ -34,7 +33,7 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children, nested =
     }, [slug, key]);
 
     const inFocus = isInFocus();
-    const [openSection, setOpenSection] = useState(false);
+    const [openSection, setOpenSection] = useState(isInFocus());
 
     const [renderComponents, setRenderComponents] = useState(false);
 
