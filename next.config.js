4459f91da40dbce3888f97518ce9b91b1406070f
Arjun Attam <arjunattam@gmail.com>
2023-01-20 17:50:19 +0530

    redirects

--- next.config.js
+++ next.config.js
@@ -151,6 +151,81 @@ const nextConfig = {
                 destination: '/javascript/v2/how--to-guides/debugging/:slug',
                 permanent: true
             },
+            {
+                source: '/javascript/v2/features/log-level',
+                destination: '/javascript/v2/how--to-guides/debugging/log-level',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/debugging/debugging',
+                destination: '/javascript/v2/how--to-guides/debugging/overview',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/advanced-features/:slug(custom-tracks|playlist)',
+                destination: '/javascript/v2/how--to-guides/extend-capabilities/custom-tracks/:slug',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/plugins/:path(.*)',
+                destination: '/javascript/v2/how--to-guides/extend-capabilities/plugins/:path(.*)',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/store-appdata',
+                destination: '/javascript/v2/how--to-guides/extend-capabilities/store-appdata',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/debugging/supported-devices',
+                destination: '/javascript/v2/how--to-guides/going-live/supported-devices',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/integration',
+                destination: '/javascript/v2/how--to-guides/install-the-sdk/integration',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/notifications',
+                destination: '/javascript/v2/how--to-guides/listen-to-room-events/notifications',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/advanced-features/:slug(connection-quality|hls-stats|stats)',
+                destination: '/javascript/v2/how--to-guides/measure-network-quality-and-performance/:slug',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/:slug(hls|rtmp-recording)',
+                destination: '/javascript/v2/how--to-guides/record-and-live-stream/:slug',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/:slug(chat|device-change|join|leave|mute|peer-name|preview|screen-share)',
+                destination: '/javascript/v2/how--to-guides/set-up-video-conferencing/:slug',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/advanced-features/audio-level',
+                destination: '/javascript/v2/how--to-guides/set-up-video-conferencing/render-video/audio-level',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/render-video',
+                destination: '/javascript/v2/how--to-guides/set-up-video-conferencing/render-video/overview',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/features/:slug(pip-mode|sub-degradation)',
+                destination: '/javascript/v2/how--to-guides/set-up-video-conferencing/render-video/:slug',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/advanced-features/simulcast',
+                destination: '/javascript/v2/how--to-guides/set-up-video-conferencing/render-video/simulcast',
+                permanent: true
+            },
         ];
     }
 };
