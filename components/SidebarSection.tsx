ecb8afdcf99c2ff4773786f2a8105c171aafb7b7
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-01-20 17:13:31 +0530

    GG-92 Nested accordions (#1154)
    * refactor sidebar
    
    * lint fix
    
    * fix
    
    * re-add server side links to sidebar
    
    * fix
    
    * collapsed header should open for active page
    
    * fix max-height
    
    * add borderRadius
    
    * handle /vx in path
    
    * fix url parsing logic
    
    * fix CSS
    
    * remove PLATFORM
    
    * fix alignment
    
    * fix border for server side features
    
    * fix tab opening
    
    * fix jitter
    
    * jitter fix
    
    * jitter fix test
    
    * persist open accordions
    
    * fix tokens
    
    * support nesting
    
    * css fix
    
    * demo file structure
    
    * fix
    
    * lint fix
    
    * update icon sizes
    
    * accordion file logic
    
    * rename index to overview
    
    * revert mapping changes
    
    * fix overflow
    
    * refactor, fix overflow
    
    * refactor, active state fix
    
    * fix
    
    * fix
    
    * fix dropdown animation
    
    * fix
    
    * refactor
    
    * add comments
    
    * animation fix
    
    * sentence casing
    
    * Handle double hyphens
    
    * inactive open accordions should not close when clicked
    
    * fix casing
    
    * Update 3 files
    
    * Delete index.mdx and test2.mdx
    
    * Update SidebarSection.tsx
    
    * revert content pages for accordions
    
    * remove link

--- components/SidebarSection.tsx
+++ components/SidebarSection.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useRef, useState, useLayoutEffect } from 'react';
+import React, { useEffect, useRef, useState, useLayoutEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import { ChevronRightIcon } from '@100mslive/react-icons';
 import { Flex, Text } from '@100mslive/react-ui';
+import SidebarItem from './SidebarItem';
+import { titleCasing } from '../lib/utils';
 
 interface Props {
     value: String;
     index: Number;
-    children: React.ReactChildren;
+    children: any;
+    nested: Boolean;
 }
 
-const SidebarSection: React.FC<Props> = ({ value: key, index, children }) => {
+const SidebarSection: React.FC<Props> = ({ value: key, index, children, nested = false }) => {
     const router = useRouter() as any;
     const {
         asPath,
@@ -18,13 +20,18 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children }) => {
     } = router;
 
     const activeItem = useRef<HTMLAnchorElement>(null);
-    const [openSection, setOpenSection] = useState(() => {
+
+    const isInFocus = useCallback(() => {
         for (const i of slug) if (i === key) return true;
         return false;
-    });
+    }, [slug, key]);
+
+    const inFocus = isInFocus();
+    const [openSection, setOpenSection] = useState(inFocus);
 
     const [renderComponents, setRenderComponents] = useState(false);
 
+    // To open accordions that were not closed before the page reload
     useLayoutEffect(() => {
         if (typeof window !== 'undefined') {
             const openedAccordions = JSON.parse(sessionStorage.getItem('openedAccordions') || '[]');
@@ -38,15 +45,18 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children }) => {
 
     useEffect(() => {
         if (window) {
+            // Add active accordions to the list - when users directly navigate via links
             const currentList = JSON.parse(sessionStorage.getItem('openedAccordions') || '[]');
             if (openSection) {
                 currentList.push(key);
                 sessionStorage.setItem('openedAccordions', JSON.stringify(currentList));
             }
+            // Styles take some time to load
             setRenderComponents(true);
         }
     }, []);
 
+    // Scroll active page into view
     useEffect(() => {
         setTimeout(() => {
             if (activeItem?.current) {
@@ -59,117 +69,91 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children }) => {
         }, 0);
     }, [activeItem]);
 
+    // const indexURL = children?.overview?.url || '';
+
     return renderComponents ? (
         <section
-            style={{ margin: '2px 0.5rem 0.5rem 0.25rem', overscrollBehavior: 'none' }}
+            style={{
+                margin: nested ? '0 0 0 0.95rem' : '2px 0.5rem 0.5rem 0.25rem',
+                borderLeft: nested ? '2px solid var(--docs_border_strong' : 'none'
+            }}
             key={`${key}-${index}`}>
-            <Flex
-                align="center"
-                onClick={() => {
-                    setOpenSection((prev) => {
-                        const currentList = JSON.parse(
-                            sessionStorage.getItem('openedAccordions') || '[]'
-                        );
-                        const updatedList = [
-                            ...new Set(
-                                prev === false
-                                    ? [...currentList, key]
-                                    : currentList.filter((heading) => heading !== key)
-                            )
-                        ];
+            {/* <ConditionalLink link={indexURL}> */}
+                <Flex
+                    align="center"
+                    onClick={() => {
+                        setOpenSection((prev) => {
+                            const currentList = JSON.parse(
+                                sessionStorage.getItem('openedAccordions') || '[]'
+                            );
+                            const updatedList = [
+                                ...new Set(
+                                    prev === false || inFocus
+                                        ? [...currentList, key]
+                                        : currentList.filter((heading) => heading !== key)
+                                )
+                            ];
+                            sessionStorage.setItem('openedAccordions', JSON.stringify(updatedList));
 
-                        sessionStorage.setItem('openedAccordions', JSON.stringify(updatedList));
-                        return !prev;
-                    });
-                }}
-                css={{
-                    marginLeft: '2rem',
-                    padding: '0.5rem 1rem',
-                    margin: '0',
-                    cursor: 'pointer',
-                    borderRadius: '$0',
-                    width: 'max-content',
-                    color: openSection ? '$textHighEmp' : '$textMedEmp',
-                    '&:hover': { color: 'var(--docs_text_primary)' }
-                }}>
-                <ChevronRightIcon
-                    style={{
-                        height: '16px',
-                        width: '18px',
-                        fontWeight: 'bold',
-                        marginRight: '0.5rem',
-                        transition: 'all 0.2s ease',
-                        transform: openSection ? 'rotateZ(90deg)' : ''
+                            return !prev;
+                        });
                     }}
-                />
-                <Text
                     css={{
-                        color: 'inherit',
-                        textTransform: 'uppercase',
-                        fontWeight: '700',
-                        fontSize: '13px',
-                        letterSpacing: '1px'
+                        padding: '0 0 0.25rem 1rem',
+                        paddingTop: nested ? '0.25rem' : '0.5rem',
+                        margin: '0',
+                        cursor: 'pointer',
+                        borderRadius: '$0',
+                        color: inFocus ? 'var(--docs_text_primary)' : 'var(--docs_text_secondary)',
+                        '&:hover': { color: '$textHighEmp' }
                     }}>
-                    {key.replace(/-/g, ' ')}
-                </Text>
-            </Flex>
-            <div className={`accordion-content ${openSection ? 'active' : ''}`}>
-                {Object.entries(children as {}).map(([_, route]: [unknown, any]) =>
+                    <ChevronRightIcon
+                        style={{
+                            height: '16px',
+                            width: '14px',
+                            minWidth: '14px',
+                            marginRight: '0.5rem',
+                            transition: 'all 0.2s ease',
+                            transform: openSection ? 'rotateZ(90deg)' : ''
+                        }}
+                    />
+                    <Text
+                        css={{
+                            color: 'inherit',
+                            fontWeight: openSection ? '600' : '500',
+                            fontSize: nested ? '13px' : '15px'
+                        }}>
+                        {titleCasing(key)}
+                    </Text>
+                </Flex>
+            {/* </ConditionalLink> */}
+            <div className={`accordion-content ${openSection ? 'active' : ''}`} style={{}}>
+                {Object.entries(children as {}).map(([_, route]: [string, any]) =>
+                    // && route.url !== indexURL ?
                     Object.prototype.hasOwnProperty.call(route, 'title') ? (
-                        <Link prefetch={false} href={route.url || ''} key={`${route.url}-${index}`}>
-                            <a
-                                ref={route.url === asPath ? activeItem : null}
-                                style={{
-                                    cursor: 'pointer',
-                                    padding: '4px 0',
-                                    color:
-                                        route.url === asPath
-                                            ? 'var(--docs_text_primary)'
-                                            : 'var(--docs_text_secondary)',
-                                    fontWeight: route.url === asPath ? '500' : '400',
-                                    fontSize: '14px',
-                                    lineHeight: '24px',
-                                    borderLeft:
-                                        route.url === asPath
-                                            ? '2px solid var(--primary_light)'
-                                            : '2px solid var(--docs_border_strong)',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    paddingLeft: '1rem',
-                                    marginLeft: '1rem'
-                                }}>
-                                {route.title}
-                            </a>
-                        </Link>
-                    ) : null
+                        <SidebarItem
+                            key={route.title}
+                            asPath={asPath}
+                            route={route}
+                            activeItem={activeItem}
+                            index={index}
+                        />
+                    ) : (
+                        <SidebarSection index={index} value={_} nested>
+                            {route}
+                        </SidebarSection>
+                    )
                 )}
                 {key === 'features' && slug[0] !== 'server-side' ? (
                     <>
-                        {aliasMenu.map((a) => (
-                            <Link scroll={false} prefetch={false} href={a.url} key={a.url}>
-                                <a
-                                    style={{
-                                        cursor: 'pointer',
-                                        padding: '4px 0',
-                                        color:
-                                            a.url === asPath
-                                                ? 'var(--docs_text_primary)'
-                                                : 'var(--docs_text_secondary)',
-                                        fontWeight: a.url === asPath ? '500' : '400',
-                                        fontSize: '14px',
-                                        lineHeight: '24px',
-                                        borderLeft:
-                                            a.url === asPath
-                                                ? '2px solid var(--primary_light)'
-                                                : '2px solid var(--docs_border_strong)',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        paddingLeft: '1rem',
-                                        marginLeft: '1rem'
-                                    }}>
-                                    {a.title}
-                                </a>
-                            </Link>
+                        {aliasMenu.map((route) => (
+                            <SidebarItem
+                                key={route.title}
+                                asPath={asPath}
+                                route={route}
+                                activeItem={activeItem}
+                                index={index}
+                            />
                         ))}
                     </>
                 ) : null}
@@ -177,15 +161,19 @@ const SidebarSection: React.FC<Props> = ({ value: key, index, children }) => {
             <style jsx>
                 {`
                     .accordion-content {
+                        position: relative;
                         margin-top: 0;
                         padding-left: 0.5rem;
-                        transition: max-height 0.3s ease;
-                        max-height: 0px;
+                        opacity: 0;
+                        max-height: 0;
+                        transition: all ease 0.3s;
                         overflow: hidden;
                     }
 
                     .active {
-                        max-height: 1000vh;
+                        top: 0;
+                        opacity: 1;
+                        max-height: 1000px;
                     }
                     a:hover {
                         opacity: 1;
