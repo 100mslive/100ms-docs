e2d70ab555d28a024d713674a605f34870da9483
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-01-27 17:12:15 +0530

    fix

--- lib/image.ts
+++ lib/image.ts
@@ -2,7 +2,7 @@ import { visit } from 'unist-util-visit'
 
 export default () => (tree) => {
     visit(tree, 'image', (node) => {
-        if (!node.url.startsWith('/docs/docs') && node.url.startsWith('/docs')) {
+        if (!node.url.startsWith('/docs/docs') && (node.url.startsWith('/docs') && !node.url.contains('/guides'))) {
             node.url = `/docs${node.url}`
         }
     })
