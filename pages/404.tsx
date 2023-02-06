a5c8182687a11ba318f73ee101d03eacbfef3125
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-02-06 09:38:53 +0530

    add server side

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,23 @@ import { Text, Flex, Box } from '@100mslive/react-ui';
 import Header from '@/components/Header';
 import Badge from '@/components/Home/Badge';
 import { SdkList } from '@/components/Home/MainCard';
+import { ServerIcon } from '@100mslive/react-icons';
 
 const NotFound = () => {
     const router = useRouter();
     const [menu, setMenu] = useState(false);
     const [modal, setModal] = useState(false);
     const menuState = { menu, setMenu };
+
+    const updatedList = [
+        ...SdkList,
+        {
+            icon: <ServerIcon color="inherit" />,
+            title: 'Server side',
+            id: 'serverSide',
+            link: '/server-side/v2/introduction/basics'
+        }
+    ];
     return (
         <Box>
             <Header
@@ -21,25 +32,44 @@ const NotFound = () => {
             />
             <Flex justify="center" css={{ pt: '$20', px: '$12' }}>
                 <Box>
-                    <Text variant="h3" css={{ color: '$textHighEmp', fontWeight: '$semiBold' }}>
+                    <Text
+                        variant="h3"
+                        css={{
+                            color: '$textHighEmp',
+                            fontWeight: '$semiBold',
+                            textAlign: 'center'
+                        }}>
                         404
                     </Text>
-                    <Text variant="h5" css={{ color: '$textHighEmp', mt: '$8' }}>
-                        Couldn't find the page you were looking for:
+                    <Text
+                        variant="h5"
+                        css={{ color: '$textHighEmp', my: '$4', textAlign: 'center' }}>
+                        The page does not exist:
                     </Text>
-                    <Text className="mono" variant="h6" css={{ mt: '$8' }}>
-                        {router.asPath.slice(1)}
+                    <Text
+                        className="mono"
+                        css={{ textAlign: 'center', color: '$textHighEmp', fontSize: '$h6' }}>
+                        {router.asPath}
                     </Text>
                     <Text
                         variant="h6"
                         css={{
                             color: '$textHighEmp',
-                            mt: '$20'
+                            my: '$12',
+                            textAlign: 'center'
                         }}>
                         Refer these links:
                     </Text>
-                    <Flex css={{ gap: '$10', mt: '$8', flexWrap: 'wrap', w: '100%' }}>
-                        {SdkList.map((sdk) => (
+                    <Flex
+                        justify="center"
+                        css={{
+                            gap: '$6',
+                            mt: '$8',
+                            flexWrap: 'wrap',
+                            w: '100%',
+                            maxWidth: '600px'
+                        }}>
+                        {updatedList.map((sdk) => (
                             <Badge key={sdk.id} {...sdk} />
                         ))}
                     </Flex>
