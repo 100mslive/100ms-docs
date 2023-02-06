eb57adafb0023f3c65f9117084ece8a705246ae4
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-02-06 15:43:52 +0530

    save

--- releases.js
+++ releases.js
@@ -1,40 +1,26 @@
-const PLATFORM_WEB = 'Web';
-const PLATFORM_SERVER = 'Server';
-const PLATFORM_IOS = 'iOS';
-const PLATFORM_ANDROID = 'Android';
-const PLATFORM_REACT_NATIVE = 'React Native';
-const PLATFORM_FLUTTER = 'Flutter';
-
-
-exports.releases = releases = [
-    {
-        platform: PLATFORM_ANDROID,
+exports.releases = releases = {
+    Android: {
         version: 'v2.5.7',
         date: 'January 20, 2023'
     },
-    {
-        platform: PLATFORM_IOS,
+    iOS: {
         version: '0.6.1',
         date: 'January 19, 2023'
     },
-    {
-        platform: PLATFORM_REACT_NATIVE,
+    'React Native': {
         version: '1.1.1',
         date: 'December 22, 2022'
     },
-    {
-        platform: PLATFORM_WEB,
+    Web: {
         version: '2023-02-03',
         date: 'February 02, 2023'
     },
-    {
-        platform: PLATFORM_FLUTTER,
+    Flutter: {
         version: '1.3.0',
         date: 'February 01, 2023'
     },
-    {
-        platform: PLATFORM_SERVER,
+    'Server-side': {
         version: '2023-01-23',
         date: 'January 23, 2023'
     }
-];
+};
