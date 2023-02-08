cea4ad515a4cd8b1ff6b9d2997a00d6718a2a882
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-02-08 17:33:07 +0530

    update release versions

--- releases.js
+++ releases.js
@@ -1,6 +1,6 @@
 exports.releases = releases = {
     Android: { version: 'v2.5.7', date: 'January 20, 2023' },
-    iOS: { version: '0.6.1', date: 'January 19, 2023' },
+    iOS: { version: '0.6.3', date: 'February 07, 2023' },
     'React Native': { version: '1.1.1', date: 'December 22, 2022' },
     Web: { version: '2023-02-03', date: 'February 02, 2023' },
     Flutter: { version: '1.3.0', date: 'February 01, 2023' },
