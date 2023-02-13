e790fea1d5ec0a105ec3432fea9fcfda6299b262
mackarelfish <mackarelfish93@gmail.com>
2023-02-13 18:08:47 +0700

    feat: category component

---
+++ components/Category.tsx
@@ -0,0 +1,71 @@
+import * as reactIcons from '@100mslive/react-icons';
+import { Box, Flex, Text } from '@100mslive/react-ui';
+import { useState } from 'react';
+
+export const CATEGORIES = {
+    ALL_CATEGORIES: 'All Categories',
+    QUICKSTARTS: 'Quickstarts',
+    APPS: 'Apps',
+    FEATURES: 'Features',
+    PLUGINS: 'Plugins',
+    EXTRAS: 'Extras'
+} as const;
+
+export type Categories = typeof CATEGORIES[keyof typeof CATEGORIES];
+
+interface Props extends React.ComponentPropsWithoutRef<typeof Flex> {
+    icon: keyof typeof reactIcons | React.ReactNode;
+    text: Categories;
+    textSmall?: string;
+    active?: boolean;
+    as?: React.ElementType;
+}
+
+export default function Category({
+    icon,
+    text,
+    textSmall = text,
+    active = false,
+    css = {},
+    as,
+    ...rest
+}: Props) {
+    const Icon = typeof icon === 'string' ? reactIcons[icon] : icon;
+    const [hover, setHover] = useState(() => active);
+
+    return (
+        <Flex
+            as={as}
+            css={{
+                color: active || hover ? '$primaryLight' : '$textHighEmp',
+                padding: '$6',
+                gap: '$4',
+                backgroundColor: active || hover ? 'hsla(217, 50%, 12%, 1)' : 'transparent',
+                borderRadius: '$1',
+                border: 'none',
+                '@sm': {
+                    paddingBlock: '$4'
+                },
+                ...css
+            }}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            {...rest}>
+            <Icon />
+            <Text
+                variant="body1"
+                css={{
+                    textShadow: active || hover ? '0px 0px 1px $primaryLight' : 'initial',
+                    fontWeight: '$regular',
+                    color: 'inherit'
+                }}>
+                <Box as="span" css={{ '@md': { display: 'none' } }}>
+                    {text}
+                </Box>
+                <Box as="span" css={{ display: 'none', '@md': { display: 'inline' } }}>
+                    {textSmall}
+                </Box>
+            </Text>
+        </Flex>
+    );
+}
