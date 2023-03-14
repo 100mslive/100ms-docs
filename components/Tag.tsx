76f02e1ac7f2a2702fa4179166cbd79fcafa3d52
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-03-14 15:17:46 +0530

    GG-242 Add type attribute to records (#1348)
    * fix
    
    * dummy commit
    
    * fix spacing for tags
    
    * fix attribution for FAQs

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,20 +1,22 @@
 import { Text } from '@100mslive/react-ui';
 
-const Tag = ({ text, css = {} }) => (
-    <Text
-        variant="xs"
-        css={{
-            backgroundColor: '$surfaceLighter',
-            borderRadius: '$0',
-            whiteSpace: 'nowrap',
-            color: '$textPrimary',
-            fontWeight: '$semiBold',
-            px: '$xs',
-            py: '$2',
-            ...css
-        }}>
-        {text}
-    </Text>
-);
+const Tag = ({ text, css = {} }) => {
+    return text ? (
+        <Text
+            variant="xs"
+            css={{
+                backgroundColor: '$surfaceLighter',
+                borderRadius: '$0',
+                whiteSpace: 'nowrap',
+                color: '$textPrimary',
+                fontWeight: '$semiBold',
+                px: '$xs',
+                py: '$2',
+                ...css
+            }}>
+            {text}
+        </Text>
+    ) : null;
+};
 
 export default Tag;
