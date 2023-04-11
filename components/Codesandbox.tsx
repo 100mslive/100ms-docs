4be4e64e18b887673a82bef8c99145ac72575825
chrisb-100ms <chris@100ms.live>
2023-04-11 16:34:52 +0400

    Fix: Sandbox top margin

--- components/Codesandbox.tsx
+++ components/Codesandbox.tsx
@@ -9,9 +9,8 @@ export default function Codesandbox({ id, tests }) {
             <LazyLoad height={400} once>
                 <iframe
                     title="Code Sandbox"
-                    src={`https://codesandbox.io/embed/${id}${paramSeparator}codemirror=1&fontsize=14&hidenavigation=1&theme=dark&hidedevtools=1${
-                        tests ? '&previewwindow=tests' : ''
-                    }`}
+                    src={`https://codesandbox.io/embed/${id}${paramSeparator}codemirror=1&fontsize=14&hidenavigation=1&theme=dark&hidedevtools=1${tests ? '&previewwindow=tests' : ''
+                        }`}
                     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
                     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
                 />
@@ -20,7 +19,7 @@ export default function Codesandbox({ id, tests }) {
                 div {
                     border: 1px solid var(--docs_border_strong);
                     border-radius: var(--docs_border_radius_s);
-                    margin-bottom: 2rem;
+                    margin-top: 16px;
                     overflow: hidden;
                 }
                 iframe {
