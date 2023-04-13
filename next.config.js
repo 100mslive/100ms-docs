10520b175f30ff3f4aee9bebdfe5ede3ee4d4312
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-04-13 18:43:21 +0530

    replace how--to with how-to, replace get started in platforms with quickstart

--- next.config.js
+++ next.config.js
@@ -1480,7 +1480,38 @@ const nextConfig = {
                 destination: '/get-started/v2/concepts/authentication/security-and-tokens',
                 permanent: true
             },
-            { source: '/concepts/:slug*', destination: '/get-started/:slug*', permanent: true }
+            { source: '/concepts/:slug*', destination: '/get-started/:slug*', permanent: true },
+            {
+                source: '/ios/v2/get-started/:slug*',
+                destination: '/ios/v2/quickstart/:slug*',
+                permanent: true
+            },
+            {
+                source: '/android/v2/get-started/:slug*',
+                destination: '/android/v2/quickstart/:slug*',
+                permanent: true
+            },
+            {
+                source: '/javascript/v2/get-started/:slug*',
+                destination: '/javascript/v2/quickstart/:slug*',
+                permanent: true
+            },
+            {
+                source: '/flutter/v2/get-started/:slug*',
+                destination: '/flutter/v2/quickstart/:slug*',
+                permanent: true
+            },
+            {
+                source: '/react-native/v2/get-started/:slug*',
+                destination: '/react-native/v2/quickstart/:slug*',
+                permanent: true
+            },
+            {
+                source: '/:slug*/how--to-guides/:path*',
+                destination: '/:slug*/how-to-guides/:path*',
+                permanent: true
+            },
+
         ];
     },
     eslint: {
