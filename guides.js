27a662ab9c5a3744d6d697bb1fdeca4739083c22
Arjun Attam <arjunattam@gmail.com>
2023-08-21 12:04:36 +0530

    fix link

--- guides.js
+++ guides.js
@@ -1,17 +1,17 @@
 export const guides = [
     {
+        title: 'Recordings',
+        subText: 'Get composite and track-level recordings for video on-demand scenarios',
+        link: '/get-started/v2/get-started/features/recordings/overview'
+    },
+    {
         title: 'Interactive Live Streaming',
         subText: 'Learn how to live stream your rooms and interact with viewers',
-        link: '/concepts/v2/concepts/live-streaming'
+        link: '/get-started/v2/get-started/features/live-streaming'
     },
     {
         title: 'Hand Raise',
         subText: 'Learn how to implement hand raise using peer metadata',
         link: '/javascript/v2/how--to-guides/build-interactive-features/peer-metadata'
-    },
-    {
-        title: 'Recordings',
-        subText: 'Get composite and track-level recordings for video on-demand scenarios',
-        link: '/concepts/v2/concepts/recordings'
     }
 ];
