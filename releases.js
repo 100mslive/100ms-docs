b376c2d6b5c85f4dcfd223b25d95d8a6b1bb6244
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-09-15 22:16:56 +0530

    fix: versions for release notes

--- releases.js
+++ releases.js
@@ -1,8 +1,1 @@
-exports.releases = releases = {
-    Android: { version: 'v2.6.8', date: 'June 28, 2023' },
-    iOS: { version: '0.9.5', date: 'June 01, 2023' },
-    'React Native': { version: '1.7.0', date: 'June 21, 2023' },
-    Web: { version: '2023-06-01', date: 'June 01, 2023' },
-    Flutter: { version: '1.7.0', date: 'June 20, 2023' },
-    'Server-side': { version: '2023-05-11', date: 'May 11, 2023' }
-};
+exports.releases = releases = {"Android":{"version":"v2.7.6 - 2023-09-14","date":"Invalid Date"},"iOS":{"version":"0.9.12","date":"September 13, 2023"},"React Native":{"version":"1.8.0 - 2023-09-15","date":"Invalid Date"},"Web":{"version":"2023-08-25","date":"August 25, 2023"},"Flutter":{"version":"1.8.0 - 2023-09-15","date":"Invalid Date"},"Server-side":{"version":"2023-08-01","date":"August 01, 2023"}}
