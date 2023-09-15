7752f2a8a642449f7a3c867ffd8d3658c2ea0826
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-09-15 22:31:53 +0530

    fix: update logic to extract date and version

--- releases.js
+++ releases.js
@@ -1,1 +1,8 @@
-exports.releases = releases = {"Android":{"version":"v2.7.6 - 2023-09-14","date":"Invalid Date"},"iOS":{"version":"0.9.12","date":"September 13, 2023"},"React Native":{"version":"1.8.0 - 2023-09-15","date":"Invalid Date"},"Web":{"version":"2023-08-25","date":"August 25, 2023"},"Flutter":{"version":"1.8.0 - 2023-09-15","date":"Invalid Date"},"Server-side":{"version":"2023-08-01","date":"August 01, 2023"}}
+exports.releases = releases = {
+    Android: { version: 'v2.7.6', date: 'September 14, 2023' },
+    iOS: { version: '0.9.12', date: 'September 13, 2023' },
+    'React Native': { version: '1.8.0', date: 'September 15, 2023' },
+    Web: { version: '2023-08-25', date: 'August 25, 2023' },
+    Flutter: { version: '1.8.0', date: 'September 15, 2023' },
+    'Server-side': { version: '2023-08-01', date: 'August 01, 2023' }
+};
