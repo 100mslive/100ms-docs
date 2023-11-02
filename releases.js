8a366cff04cfc1d320cd4a8872c8791fd48211af
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-11-02 10:14:22 +0530

    Rename changelog (#1829)
    * fix: rename changelog to release-notes
    
    * fix: redirect regex
    
    * fix: script to update release-notes

--- releases.js
+++ releases.js
@@ -1,1 +1,8 @@
-exports.releases = releases = {"Android":{"version":"v2.7.7","date":"October 04, 2023"},"iOS":{"version":"1.1.0","date":"September 28, 2023"},"React Native":{"version":"1.9.0","date":"October 12, 2023"},"Web":{"version":"2023-10-04","date":"October 04, 2023"},"Flutter":{"version":"1.9.0","date":"October 16, 2023"},"Server-side":{"version":"2023-10-17","date":"October 17, 2023"}}
+exports.releases = releases = {
+    Android: { version: 'v2.7.7', date: 'October 04, 2023' },
+    iOS: { version: '1.1.0', date: 'September 28, 2023' },
+    'React Native': { version: '1.9.0', date: 'October 12, 2023' },
+    Web: { version: '2023-10-04', date: 'October 04, 2023' },
+    Flutter: { version: '1.9.0', date: 'October 16, 2023' },
+    'Server-side': { version: '2023-10-17', date: 'October 17, 2023' }
+};
