87642bff0dc399d72c8908e083e596b717596b27
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-04 14:24:27 +0530

    fix: script for updating release versions

--- releases.js
+++ releases.js
@@ -1,8 +1,1 @@
-exports.releases = releases = {
-    Android: { version: 'v2.7.7', date: 'October 04, 2023' },
-    iOS: { version: '1.1.0', date: 'September 28, 2023' },
-    'React Native': { version: '1.9.0', date: 'October 12, 2023' },
-    Web: { version: '2023-10-04', date: 'October 04, 2023' },
-    Flutter: { version: '1.9.0', date: 'October 16, 2023' },
-    'Server-side': { version: '2023-10-17', date: 'October 17, 2023' }
-};
+exports.releases = releases = {"Android":{"version":"v2.8.0","date":"November 03, 2023"},"iOS":{"version":"1.2.0","date":"November 03, 2023"},"React Native":{"version":"1.9.1","date":"November 03, 2023"},"Web":{"version":"2023-10-04","date":"October 04, 2023"},"Flutter":{"version":"1.9.1","date":"November 03, 2023"},"Server-side":{"version":"2023-10-31","date":"October 31, 2023"}}
