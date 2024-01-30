3a0c074db7008b562bbff56a6e79affaf8d2e398
AnkitRastogi3 <ankit.rastogi@100ms.live>
2024-01-30 14:33:08 +0530

    Changed bullet points and added analytics in suggested blog click

--- components/SuggestedBlogs.tsx
+++ components/SuggestedBlogs.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Box, Text } from '@100mslive/react-ui';
 import { ExternalLinkIcon } from '@100mslive/react-icons';
-import Link from 'next/link';
-
 interface Props {
     suggestedBlogs: Array<{
         title: string;
@@ -42,7 +40,7 @@ const SuggestedBlogs: React.FC<Props> = ({ suggestedBlogs = suggestedBlogsDefaul
                 css={{
                     width: '100%',
                     display: 'grid',
-                    gridGap: '$12',
+                    gridGap: '$8',
                     gridTemplateColumns: '1fr 1fr 1fr',
                     '@xl': {
                         gridTemplateColumns: '1fr 1fr'
@@ -53,83 +51,90 @@ const SuggestedBlogs: React.FC<Props> = ({ suggestedBlogs = suggestedBlogsDefaul
                 }}>
                 {suggestedBlogs?.map((blog, index, array) => {
                     return (
-                        <Link href={blog.url}>
+                        <Box
+                            css={{
+                                position: 'relative',
+                                background:
+                                    'linear-gradient(280deg, rgba(18, 22, 27, 0.80) 37.76%, rgba(22, 27, 34, 0.80) 91.22%)',
+                                flexGrow: 1,
+                                borderRadius: '16px',
+                                padding: '12px 24px 16px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                cursor: 'pointer',
+                                width: '100%',
+                                '@xl': {
+                                    gridColumn: index === array.length - 1 ? '1 / -1' : ''
+                                },
+                                '@md': {
+                                    maxWidth: 'unset',
+                                    height: 'unset'
+                                },
+                                '&:hover': {
+                                    background: 'var(--docs_bg_home_page_card_hover)'
+                                }
+                            }}
+                            onClick={() => {
+                                window.analytics.track('docs.blog.redirect', {
+                                    type: 'blog_redirect',
+                                    blog_title: blog.title,
+                                    path: window.location.pathname,
+                                    timeStamp: new Date().toLocaleString(),
+                                    blog_url: blog.url
+                                });
+                                window.open(blog.url, '_blank');
+                            }}>
                             <Box
                                 css={{
-                                    position: 'relative',
-                                    background:
-                                        'linear-gradient(280deg, rgba(18, 22, 27, 0.80) 37.76%, rgba(22, 27, 34, 0.80) 91.22%)',
-                                    flexGrow: 1,
-                                    borderRadius: '16px',
-                                    padding: '12px 24px 16px',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'space-between',
-                                    cursor: 'pointer',
-                                    width: '100%',
-                                    '@xl': {
-                                        gridColumn: index === array.length - 1 ? '1 / -1' : ''
-                                    },
-                                    '@md': {
-                                        maxWidth: 'unset',
-                                        height: 'unset'
-                                    },
-                                    '&:hover': {
-                                        background: 'var(--docs_bg_home_page_card_hover)'
-                                    }
+                                    width: '$7',
+                                    position: 'absolute',
+                                    top: '12px',
+                                    right: '16px'
                                 }}>
-                                <Box
-                                    css={{
-                                        width: '$7',
-                                        position: 'absolute',
-                                        top: '12px',
-                                        right: '16px'
-                                    }}>
-                                    <ExternalLinkIcon style={{ width: '100%' }} />
-                                </Box>
-                                <Box css={{ maxWidth: '80%', marginBottom: '$6' }}>
-                                    <Text
-                                        variant={'h6'}
-                                        style={{
-                                            display: '-webkit-box',
-                                            WebkitBoxOrient: 'vertical',
-                                            boxOrient: 'vertical',
-                                            WebkitLineClamp: '3',
-                                            lineClamp: '3',
-                                            overflow: 'hidden',
-                                            textOverflow: 'ellipsis'
-                                        }}
-                                        css={{ color: '$textPrimary' }}>
-                                        {blog.title}
-                                    </Text>
-                                    <Text
-                                        variant={'sm'}
-                                        style={{
-                                            display: '-webkit-box',
-                                            WebkitBoxOrient: 'vertical',
-                                            boxOrient: 'vertical',
-                                            WebkitLineClamp: '2',
-                                            lineClamp: '2',
-                                            overflow: 'hidden',
-                                            textOverflow: 'ellipsis'
-                                        }}
-                                        css={{
-                                            color: '$secondaryLight'
-                                        }}>
-                                        {blog.content}
-                                    </Text>
-                                </Box>
-                                <Box>
-                                    <Box
-                                        css={{ height: '1px', background: '$secondaryGray' }}></Box>
-                                    <Text
-                                        variant={'tiny'}
-                                        css={{ color: '$secondaryLight', maxWidth: '80%' }}>
-                                        {blog.date}
-                                    </Text>
-                                </Box>
+                                <ExternalLinkIcon style={{ width: '100%' }} />
                             </Box>
-                        </Link>
+                            <Box css={{ maxWidth: '80%', marginBottom: '$6' }}>
+                                <Text
+                                    variant={'h6'}
+                                    style={{
+                                        display: '-webkit-box',
+                                        WebkitBoxOrient: 'vertical',
+                                        boxOrient: 'vertical',
+                                        WebkitLineClamp: '3',
+                                        lineClamp: '3',
+                                        overflow: 'hidden',
+                                        textOverflow: 'ellipsis'
+                                    }}
+                                    css={{ color: '$textPrimary' }}>
+                                    {blog.title}
+                                </Text>
+                                <Text
+                                    variant={'sm'}
+                                    style={{
+                                        display: '-webkit-box',
+                                        WebkitBoxOrient: 'vertical',
+                                        boxOrient: 'vertical',
+                                        WebkitLineClamp: '2',
+                                        lineClamp: '2',
+                                        overflow: 'hidden',
+                                        textOverflow: 'ellipsis'
+                                    }}
+                                    css={{
+                                        color: '$secondaryLight'
+                                    }}>
+                                    {blog.content}
+                                </Text>
+                            </Box>
+                            <Box>
+                                <Box css={{ height: '1px', background: '$secondaryGray' }}></Box>
+                                <Text
+                                    variant={'tiny'}
+                                    css={{ color: '$secondaryLight', maxWidth: '80%' }}>
+                                    {blog.date}
+                                </Text>
+                            </Box>
+                        </Box>
                     );
                 })}
             </Box>
