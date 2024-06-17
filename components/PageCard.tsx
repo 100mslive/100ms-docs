febac9c9c339f555f25ff7aa695e87c0cdcf3bbd
Aditya Thakur <adityathakurxd@gmail.com>
2024-06-17 14:07:18 +0530

    Fix the link and the tag text

--- components/PageCard.tsx
+++ components/PageCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Flex, Text } from '@100mslive/react-ui';
 
 import { DynamicIcon } from './Callout';
+import { ArrowRightIcon } from '@100mslive/react-icons';
 
 const PageCard = ({
     showCta = true,
@@ -51,14 +52,17 @@ const PageCard = ({
                     marginTop: '24px',
                     padding: '4px 8px',
                     textAlign: 'center',
-                    borderRadius: '4px'
+                    borderRadius: '4px',
+                    alignItems: 'center'
                 }}>
                 <Text
-                    style={{
-                        marginTop: '0 !important',
+                    css={{
+                        margin: '0 !important',
                         color: 'white',
                         lineHeight: '0.5rem',
-                        padding: '4px 6px'
+                        padding: '4px 8px',
+                        fontSize: '14px',
+                        fontWeight: '600'
                     }}>
                     Tag Text
                 </Text>
@@ -97,10 +101,11 @@ const PageCard = ({
             </Text>
             {showCta && (
                 <Flex
+                    gap={1}
                     style={{
                         paddingTop: '12px',
                         paddingBottom: '12px',
-                        textAlign: 'center',
+
                         backgroundColor: 'var(--new_surface_lighter)',
                         borderRadius: '0px 0px 16px  16px'
                     }}>
@@ -108,11 +113,30 @@ const PageCard = ({
                         href="#"
                         className="cta"
                         style={{
-                            color: '#66A1FF',
                             textDecoration: 'none',
                             paddingLeft: '24px'
                         }}>
-                        Read More
+                        <Flex
+                            gap={1}
+                            style={{
+                                alignItems: 'center'
+                            }}>
+                            <Text
+                                css={{
+                                    color: 'var(--selector_blue)',
+                                    fontSize: '14px',
+                                    fontWeight: '600',
+                                    margin: '0 !important',
+                                    padding: '0 !important'
+                                }}>
+                                Read More
+                            </Text>
+                            <ArrowRightIcon
+                                height="16px"
+                                width="16px"
+                                color="var(--selector_blue)"
+                            />
+                        </Flex>
                     </a>
                 </Flex>
             )}
