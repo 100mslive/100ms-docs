b6d80259b95c4c70f78b90df60ed73404d3ea474
Aditya Thakur <adityathakurxd@gmail.com>
2024-06-17 16:54:52 +0530

    Fixes issue with transition and add showTag bool

--- components/PageCard.tsx
+++ components/PageCard.tsx
@@ -6,7 +6,8 @@ import { ArrowRightIcon } from '@100mslive/react-icons';
 
 const PageCard = ({
     showCta = true,
-    showImage = true,
+    showTag = false,
+    showImage = false,
     icon,
     color = '$textHighEmp',
     title,
@@ -38,74 +39,86 @@ const PageCard = ({
                     style={{
                         borderRadius: '16px 16px 0px 0px',
                         height: 'auto',
-
                         aspectRatio: '16/9',
-                        padding: '0px !important'
+                        padding: '0px !important',
+                        transition: 'transform 0.3s',
+                        transform: hover ? 'translateY(9rem)' : 'translateY(0)'
                     }}
                 />
             )}
-            <Flex
-                style={{
-                    background: 'var(--selector_blue)',
-                    width: 'fit-content',
-                    marginLeft: '24px',
-                    marginTop: '24px',
-                    padding: '4px 8px',
-                    textAlign: 'center',
-                    borderRadius: '4px',
-                    alignItems: 'center'
-                }}>
-                <Text
-                    css={{
-                        margin: '0 !important',
-                        color: 'white',
-                        lineHeight: '0.5rem',
-                        padding: '4px 8px',
-                        fontSize: '14px',
-                        fontWeight: '600'
-                    }}>
-                    Tag Text
-                </Text>
-            </Flex>
 
             <Flex
                 style={{
                     marginTop: '12px',
                     marginLeft: '22px',
-                    textAlign: 'center'
+                    transition: 'transform 0.3s',
+                    transform: showImage
+                        ? hover
+                            ? 'translateY(-12rem)'
+                            : 'translateY(0)'
+                        : 'translateY(0)'
                 }}
-                gap={'2'}
-                align={'center'}>
-                <DynamicIcon name={icon} color={color} />
+                direction={'column'}>
+                {showTag && (
+                    <Flex
+                        style={{
+                            background: 'var(--selector_blue)',
+                            width: 'fit-content',
+                            marginTop: showImage ? '24px' : '0',
+                            padding: '4px 8px',
+                            textAlign: 'center',
+                            borderRadius: '4px',
+                            alignItems: 'center'
+                        }}>
+                        <Text
+                            css={{
+                                margin: '0 !important',
+                                color: 'white',
+                                lineHeight: '0.5rem',
+                                padding: '4px 8px',
+                                fontSize: '14px',
+                                fontWeight: '600'
+                            }}>
+                            Tag Text
+                        </Text>
+                    </Flex>
+                )}
+                <Flex
+                    style={{
+                        textAlign: 'center',
+                        marginTop: '12px'
+                    }}
+                    gap={'2'}
+                    align={'center'}>
+                    <DynamicIcon name={icon} color={color} />
+                    <Text
+                        css={{
+                            fontSize: '16px',
+                            fontWeight: 'bold',
+                            color: 'var(--white)',
+                            marginTop: '0 !important',
+                            paddingBottom: '0 !important'
+                        }}>
+                        {title}
+                    </Text>
+                </Flex>
                 <Text
                     css={{
-                        fontSize: '16px',
-                        fontWeight: 'bold',
-                        color: 'var(--white)',
-                        marginTop: '0 !important',
-                        paddingBottom: '0 !important'
-                    }}>
-                    {title}
+                        fontSize: '14px',
+                        color: 'var(--text_med_emp)',
+                        marginTop: '4px !important',
+                        marginBottom: showCta ? '12px !important' : '24px !important'
+                    }}
+                    className={`description ${hover ? 'hover' : ''}`}>
+                    {description}
                 </Text>
             </Flex>
-            <Text
-                css={{
-                    fontSize: '14px',
-                    color: 'var(--text_med_emp)',
-                    marginTop: '4px !important',
-                    marginBottom: showCta ? '12px !important' : '24px !important',
-                    paddingLeft: '24px'
-                }}
-                className={`description ${hover ? 'hover' : ''}`}>
-                {description}
-            </Text>
             {showCta && (
                 <Flex
                     gap={1}
                     style={{
                         paddingTop: '12px',
                         paddingBottom: '12px',
-
                         backgroundColor: 'var(--new_surface_lighter)',
                         borderRadius: '0px 0px 16px  16px'
                     }}>
