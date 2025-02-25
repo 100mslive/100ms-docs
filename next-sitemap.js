43457611a19ed2dad7c2f49480cc02f6f413cb97
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2025-02-25 15:33:05 +0530

    fix: sitemap, hms references

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,6 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://docs.100ms.live/',
-    exclude: ['/api-reference/*']
+    exclude: ['/api-reference/*'],
+    changefreq: "weekly",
+    priority: 0.6
 };
